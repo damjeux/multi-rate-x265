f576408934e97c546994ed1b881eb3041d4886ed
Mandar Gurav <Mandar Gurav@localhost>
2013-04-23 15:59:18 -0700

    Implementation of SAD using intrinsics for 8 bit values.

---
+++ source/input/yuv.cpp
@@ -0,0 +1,90 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#include "yuv.h"
+#include "PPA/ppa.h"
+#include <stdio.h>
+#include <string.h>
+
+using namespace x265;
+
+YUVInput::YUVInput(const char *filename)
+{
+    fp = fopen(filename, "rb");
+    width = height = 0;
+    depth = 8;
+    buf = NULL;
+}
+
+YUVInput::~YUVInput()
+{
+    if (fp) fclose(fp);
+    if (buf) delete[] buf;
+}
+
+int  YUVInput::guessFrameCount() const
+{
+    /* TODO: Get file size, divide by bufsize */
+    return 0;
+}
+
+void YUVInput::skipFrames(int numFrames)
+{
+    int pixelbytes = depth > 8 ? 2 : 1;
+
+    int framesize = (width * height * 3 / 2) * pixelbytes;
+
+    fseek(fp, framesize * numFrames, SEEK_CUR);
+}
+
+// TODO: only supports 4:2:0 chroma sampling
+bool YUVInput::readPicture(x265_picture& pic)
+{
+    PPAStartCpuEventFunc(read_yuv);
+
+    int pixelbytes = depth > 8 ? 2 : 1;
+
+    int bufsize = (width * height * 3 / 2) * pixelbytes;
+
+    if (!buf)
+    {
+        buf = new uint8_t[bufsize];
+    }
+
+    pic.planes[0] = buf;
+
+    pic.planes[1] = (char*)(pic.planes[0]) + width * height * pixelbytes;
+
+    pic.planes[2] = (char*)(pic.planes[1]) + ((width * height * pixelbytes) >> 2);
+
+    pic.bitDepth = depth;
+
+    pic.stride[0] = width * pixelbytes;
+
+    pic.stride[1] = pic.stride[2] = pic.stride[0] >> 1;
+
+    size_t bytes = fread(buf, 1, bufsize, fp);
+    PPAStopCpuEventFunc(read_yuv);
+
+    return bytes == (size_t)bufsize;
+}
---
+++ source/input/yuv.h
@@ -0,0 +1,86 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#ifndef _YUV_H_
+#define _YUV_H_
+
+#include "input.h"
+#include <stdio.h>
+#include <stdint.h>
+
+namespace x265 {
+// private x265 namespace
+
+class YUVInput : public Input
+{
+protected:
+
+    int width;
+
+    int height;
+
+    int depth;
+
+    uint8_t* buf;
+
+    FILE *fp;
+
+    bool eof;
+
+public:
+
+    YUVInput(const char *filename);
+
+    virtual ~YUVInput();
+
+    void setDimensions(int w, int h)              { width = w; height = h; }
+
+    void setBitDepth(int bitDepth)                { depth = bitDepth; }
+
+    float getRate() const                         { return 0.0f; }
+
+    int getWidth() const                          { return width; }
+
+    int getHeight() const                         { return height; }
+
+    bool isEof() const                            { return !!feof(fp); }
+
+    bool isFail() const                           { return !fp; }
+
+    void release()
+    {
+        if (fp)
+            fclose(fp);
+
+        delete this;
+    }
+
+    int  guessFrameCount() const;
+
+    void skipFrames(int numFrames);
+
+    bool readPicture(x265_picture&);
+};
+}
+
+#endif // _YUV_H_
