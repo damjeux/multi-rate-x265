f70516293d150345c0cbd0304cb2e8a3cf1265df
Steve Borho <steve@borho.org>
2013-05-06 09:56:45 -0500

    TEncSlice: backout replacement of calcRdCost64 with CALCRDCOST_SAD
    Those were two entirely different function calls

---
+++ source/encoder/bitcost.h
@@ -0,0 +1,76 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#ifndef __BITCOST__
+#define __BITCOST__
+
+#include "threading.h"
+#include "mv.h"
+
+#include <stdint.h>
+
+namespace x265 {
+// private x265 namespace
+
+class BitCost
+{
+public:
+
+    BitCost() : cost_mvx(0), cost_mvy(0), cost(0) {}
+
+    void setQP(unsigned int qp, double lambda);
+
+    void setMVP(const MV& mvp)               { cost_mvx = cost - mvp.x; cost_mvy = cost - mvp.y; }
+
+    // return bit cost of absolute motion vector
+    uint16_t mvcost(const MV& mv) const      { return cost_mvx[mv.x] + cost_mvy[mv.y]; }
+
+    static void destroy();
+
+protected:
+
+    uint16_t *cost_mvx;
+
+    uint16_t *cost_mvy;
+
+    uint16_t *cost;
+
+    BitCost& operator=(const BitCost&);
+
+private:
+
+    static const int BC_MAX_MV = 0x8000;
+
+    static const int BC_MAX_QP = 82;
+
+    static float *logs;
+
+    static uint16_t *costs[BC_MAX_QP];
+
+    static Lock costCalcLock;
+
+    static void CalculateLogs();
+};
+}
+
+#endif // ifndef __BITCOST__
