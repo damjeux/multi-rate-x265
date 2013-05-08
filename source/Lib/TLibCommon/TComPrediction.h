1c268af97c6e98c70ee2545df7c492d02e774977
Mandar Gurav <Mandar Gurav@localhost>
2013-05-07 23:20:42 -0700

    Script for generating dr_psnr output. Output files are as follows * encoder_output.txt * decoder_output.txt * dr_psnr_output.txt

---
+++ source/Lib/TLibCommon/TComPrediction.h
@@ -0,0 +1,116 @@
+/* The copyright in this software is being made available under the BSD
+ * License, included below. This software may be subject to other third party
+ * and contributor rights, including patent rights, and no such rights are
+ * granted under this license.
+ *
+ * Copyright (c) 2010-2013, ITU/ISO/IEC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  * Neither the name of the ITU/ISO/IEC nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \file     TComPrediction.h
+    \brief    prediction class (header)
+*/
+
+#ifndef __TCOMPREDICTION__
+#define __TCOMPREDICTION__
+
+// Include files
+#include "TComPic.h"
+#include "TComMotionInfo.h"
+#include "TComPattern.h"
+#include "TComTrQuant.h"
+#include "TComInterpolationFilter.h"
+#include "TComWeightPrediction.h"
+#include "TShortYUV.h"
+
+//! \ingroup TLibCommon
+//! \{
+
+// ====================================================================================================================
+// Class definition
+// ====================================================================================================================
+
+/// prediction class
+class TComPrediction : public TComWeightPrediction
+{
+protected:
+
+    Pel*      m_piPredBuf;
+    Int       m_iPredBufStride;
+    Int       m_iPredBufHeight;
+
+    TComYuv   m_acYuvPred[2];
+    TComYuv   m_cYuvPredTemp;
+    /*This holds final interpolated pixel values (0-255). Hence memory is stored as Pel.*/
+    TComYuv m_filteredBlock[4][4]; 
+    /*This holds intermediate values for filtering operations which need to maintain Short precision*/
+    TShortYUV filteredBlockTmp[4];//This 
+
+    TComInterpolationFilter m_if;
+
+    Pel*   m_pLumaRecBuffer;     ///< array for downsampled reconstructed luma sample
+    Int    m_iLumaRecStride;     ///< stride of #m_pLumaRecBuffer array
+
+    // motion compensation functions
+    Void xPredInterUni(TComDataCU* pcCU,                          UInt uiPartAddr,               Int iWidth, Int iHeight, RefPicList eRefPicList, TComYuv*& rpcYuvPred, Bool bi = false);
+    Void xPredInterBi(TComDataCU* pcCU,                          UInt uiPartAddr,               Int iWidth, Int iHeight,                         TComYuv*& rpcYuvPred);
+    Void xPredInterLumaBlk(TComDataCU *cu, TComPicYuv *refPic, UInt partAddr, TComMv *mv, Int width, Int height, TComYuv *&dstPic, Bool bi);
+    Void xPredInterChromaBlk(TComDataCU *cu, TComPicYuv *refPic, UInt partAddr, TComMv *mv, Int width, Int height, TComYuv *&dstPic, Bool bi);
+    Void xWeightedAverage(TComYuv* pcYuvSrc0, TComYuv* pcYuvSrc1, Int iRefIdx0, Int iRefIdx1, UInt uiPartAddr, Int iWidth, Int iHeight, TComYuv*& rpcYuvDst);
+
+    Void xGetLLSPrediction(TComPattern* pcPattern, Int* pSrc0, Int iSrcStride, Pel* pDst0, Int iDstStride, UInt uiWidth, UInt uiHeight, UInt uiExt0);
+
+    Bool xCheckIdenticalMotion(TComDataCU* pcCU, UInt PartAddr);
+
+public:
+
+    TComPrediction();
+    virtual ~TComPrediction();
+
+    Void    initTempBuff();
+
+    // inter
+    Void motionCompensation(TComDataCU* pcCU, TComYuv* pcYuvPred, RefPicList eRefPicList = REF_PIC_LIST_X, Int iPartIdx = -1);
+
+    // motion vector prediction
+    Void getMvPredAMVP(TComDataCU* pcCU, UInt uiPartIdx, UInt uiPartAddr, RefPicList eRefPicList, TComMv& rcMvPred);
+
+    // Angular Intra
+    Void predIntraLumaAng(TComPattern* pcTComPattern, UInt uiDirMode, Pel* piPred, UInt uiStride, Int iWidth, Int iHeight, Bool bAbove, Bool bLeft);
+    Void predIntraChromaAng(Pel* piSrc, UInt uiDirMode, Pel* piPred, UInt uiStride, Int iWidth, Int iHeight, Bool bAbove, Bool bLeft);
+
+
+    Pel* getPredicBuf()             { return m_piPredBuf; }
+
+    Int  getPredicBufWidth()        { return m_iPredBufStride; }
+
+    Int  getPredicBufHeight()       { return m_iPredBufHeight; }
+};
+
+//! \}
+
+#endif // __TCOMPREDICTION__
---
+++ source/test/testharness.h
@@ -0,0 +1,95 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#ifndef _TESTHARNESS_H_
+#define _TESTHARNESS_H_ 1
+
+#include "primitives.h"
+#include <intrin.h>
+#include <stddef.h>
+
+#if HIGH_BIT_DEPTH
+#define BIT_DEPTH 10
+#else
+#define BIT_DEPTH 8
+#endif
+#define PIXEL_MAX ((1 << BIT_DEPTH) - 1)
+
+class TestHarness
+{
+public:
+
+    TestHarness() {}
+
+    virtual ~TestHarness() {}
+
+    virtual bool testCorrectness(const x265::EncoderPrimitives& ref, const x265::EncoderPrimitives& opt) = 0;
+
+    virtual void measureSpeed(const x265::EncoderPrimitives& ref, const x265::EncoderPrimitives& opt) = 0;
+
+    static void *alignedMalloc(size_t size, int count, int alignment);
+
+    static void alignedFree(void *ptr);
+};
+
+class Timer
+{
+public:
+
+    Timer() {}
+
+    virtual ~Timer() {}
+
+    static Timer *CreateTimer();
+
+    virtual void Start() = 0;
+
+    virtual void Stop() = 0;
+
+    virtual uint64_t Elapsed() = 0;
+
+    virtual void Release() = 0;
+};
+
+static inline uint64_t read_time(void)
+{
+    unsigned __int64 i=0;
+#ifdef _MSC_VER    
+    i = __rdtsc();
+#endif
+    return i;
+}
+
+#define REPORT_SPEEDUP(ITERATIONS, RUNOPT, RUNREF) \
+{ \
+    t->Start(); unsigned __int64 ticks1 = read_time(); for (int X=0; X < ITERATIONS; X++) { RUNOPT; } unsigned __int64 ticks2 = read_time(); t->Stop(); \
+    uint64_t optelapsed = t->Elapsed(); \
+    uint64_t optelapsed2 = ticks2 - ticks1; \
+    ticks1 = read_time(); t->Start(); for (int X=0; X < ITERATIONS; X++) { RUNREF; } t->Stop(); ticks2 = read_time(); \
+    uint64_t refelapsed = t->Elapsed(); \
+    uint64_t refelapsed2 = ticks2 - ticks1; \
+    printf("\t%3.2fx ", (double)refelapsed/optelapsed ); \
+    printf("\t %.2lf \t %.2lf\n", (double)optelapsed2/ITERATIONS, (double)refelapsed2/ITERATIONS); \
+}
+
+#endif // ifndef _TESTHARNESS_H_
