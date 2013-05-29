bb0c7b8f193fb283ab9a5ac9f6dbc48b2bf44914
Deepthi <Deepthi@localhost>
2013-05-29 20:33:00 +0530

    ME satd costs and bits captured. FAST_MODE_DECISION introduced.

---
+++ source/common/common.cpp
@@ -0,0 +1,309 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Deepthi Nandakumar <deepthi@multicorewareinc.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#include "x265.h"
+#include "common.h"
+#include "TLibCommon/TComRom.h"
+#include "TLibCommon/TComSlice.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#if HIGH_BIT_DEPTH
+const int x265_bit_depth = 10;
+#else
+const int x265_bit_depth = 8;
+#endif
+
+extern "C"
+void x265_param_default( x265_param_t *param )
+{
+    memset(param, 0, sizeof(x265_param_t));
+    param->searchMethod = X265_STAR_SEARCH;
+    param->iSearchRange = 64;
+    param->bipredSearchRange = 4;
+    param->iIntraPeriod = 16;
+    param->internalBitDepth = 8;
+    param->uiMaxCUSize = 64;
+    param->uiMaxCUDepth = 4;
+    param->uiQuadtreeTULog2MaxSize = 5;
+    param->uiQuadtreeTULog2MinSize = 2;
+    param->uiQuadtreeTUMaxDepthInter = 3;
+    param->uiQuadtreeTUMaxDepthIntra = 3;
+    param->enableAMP = 1;
+    param->enableRectInter = 1;
+    param->iQP = 32;
+    param->iQPAdaptationRange = 6;
+    param->bUseSAO = 0;
+    param->maxNumOffsetsPerPic = 2048;
+    param->saoLcuBasedOptimization = 1;
+    param->log2ParallelMergeLevel = 2;
+    param->maxNumMergeCand = 5u;
+    param->TMVPModeId = 1;
+    param->signHideFlag = 1;
+    param->useFastDecisionForMerge = 1;
+    param->useStrongIntraSmoothing = 1;
+    param->useRDOQ = 1;
+    param->useRDOQTS = 1;
+    param->useTransformSkip = 1;
+    param->useTransformSkipFast = 1;
+}
+
+extern "C"
+int x265_param_apply_profile( x265_param_t *param, const char *profile )
+{
+    if (!profile)
+        return 0;
+    if (!strcmp(profile, "main"))
+    {
+        param->iIntraPeriod = 16;
+    }
+    else if (!strcmp(profile, "main10"))
+    {
+        param->iIntraPeriod = 16;
+#if HIGH_BIT_DEPTH
+        param->internalBitDepth = 10;
+#else
+        fprintf(stderr, "x265: ERROR. not compiled for 16bpp. Falling back to main profile.\n");
+        return -1;
+#endif
+    }
+    else if (!strcmp(profile, "mainstillpicture"))
+    {
+        param->iIntraPeriod = 1;
+    }
+    else
+    {
+        fprintf(stderr, "x265: ERROR. unknown profile <%s>\n", profile);
+        return -1;
+    }
+
+    return 0;
+}
+
+int dumpBuffer(void * pbuf, size_t bufsize, const char * filename)
+{
+    const char * mode = "wb";
+
+    FILE * fp = fopen(filename, mode);
+    if(!fp)
+    {
+        printf("ERROR: dumpBuffer: fopen('%s','%s') failed\n", filename, mode); return -1;
+    }
+    fwrite(pbuf, 1, bufsize, fp);
+    fclose(fp);
+    printf("dumpBuffer: dumped %8ld bytes into %s\n", (long)bufsize, filename);
+    return 0;
+}
+
+static inline int _confirm(bool bflag, const char* message)
+{
+    if (!bflag)
+        return 0;
+
+    printf("Error: %s\n", message);
+    return 1;
+}
+
+int x265_check_params(x265_param_t *param)
+{
+#define CONFIRM(expr, msg) check_failed |= _confirm(expr, msg)
+    int check_failed = 0; /* abort if there is a fatal configuration problem */
+
+#if !HIGH_BIT_DEPTH
+    CONFIRM(param->internalBitDepth != 8,
+        "InternalBitDepth must be 8");
+#endif
+    CONFIRM(param->iQP <  -6 * (param->internalBitDepth - 8) || param->iQP > 51,
+        "QP exceeds supported range (-QpBDOffsety to 51)");
+    CONFIRM(param->iFrameRate <= 0,
+        "Frame rate must be more than 1");
+    CONFIRM(param->searchMethod < 0 || param->searchMethod > X265_ORIG_SEARCH,
+        "Search method is not supported value (0:DIA 1:HEX 2:UMH 3:HM 4:ORIG)");
+    CONFIRM(param->iSearchRange < 0,
+        "Search Range must be more than 0");
+    CONFIRM(param->bipredSearchRange < 0,
+        "Search Range must be more than 0");
+    CONFIRM(param->iMaxCuDQPDepth > param->uiMaxCUDepth - 1,
+        "Absolute depth for a minimum CuDQP exceeds maximum coding unit depth");
+
+    CONFIRM(param->cbQpOffset < -12,   "Min. Chroma Cb QP Offset is -12");
+    CONFIRM(param->cbQpOffset >  12,   "Max. Chroma Cb QP Offset is  12");
+    CONFIRM(param->crQpOffset < -12,   "Min. Chroma Cr QP Offset is -12");
+    CONFIRM(param->crQpOffset >  12,   "Max. Chroma Cr QP Offset is  12");
+
+    CONFIRM(param->iQPAdaptationRange <= 0,
+        "QP Adaptation Range must be more than 0");
+    CONFIRM((param->uiMaxCUSize >> param->uiMaxCUDepth) < 4,
+        "Minimum partition width size should be larger than or equal to 8");
+    CONFIRM(param->uiMaxCUSize < 16,
+        "Maximum partition width size should be larger than or equal to 16");
+    CONFIRM((param->iSourceWidth  % (param->uiMaxCUSize >> (param->uiMaxCUDepth - 1))) != 0,
+        "Resulting coded frame width must be a multiple of the minimum CU size");
+    CONFIRM((param->iSourceHeight % (param->uiMaxCUSize >> (param->uiMaxCUDepth - 1))) != 0,
+        "Resulting coded frame height must be a multiple of the minimum CU size");
+
+    CONFIRM(param->uiQuadtreeTULog2MinSize < 2,
+        "QuadtreeTULog2MinSize must be 2 or greater.");
+    CONFIRM(param->uiQuadtreeTULog2MaxSize > 5,
+        "QuadtreeTULog2MaxSize must be 5 or smaller.");
+    CONFIRM((1u << param->uiQuadtreeTULog2MaxSize) > param->uiMaxCUSize,
+        "QuadtreeTULog2MaxSize must be log2(maxCUSize) or smaller.");
+
+    CONFIRM(param->uiQuadtreeTULog2MaxSize < param->uiQuadtreeTULog2MinSize,
+        "QuadtreeTULog2MaxSize must be greater than or equal to m_uiQuadtreeTULog2MinSize.");
+    CONFIRM((1u << param->uiQuadtreeTULog2MinSize) > (param->uiMaxCUSize >> (param->uiMaxCUDepth - 1)),
+        "QuadtreeTULog2MinSize must not be greater than minimum CU size"); // HS
+    CONFIRM((1u << param->uiQuadtreeTULog2MinSize) > (param->uiMaxCUSize >> (param->uiMaxCUDepth - 1)),
+        "QuadtreeTULog2MinSize must not be greater than minimum CU size"); // HS
+    CONFIRM((1u << param->uiQuadtreeTULog2MinSize) > (param->uiMaxCUSize >> param->uiMaxCUDepth),
+        "Minimum CU width must be greater than minimum transform size.");
+    CONFIRM((1u << param->uiQuadtreeTULog2MinSize) > (param->uiMaxCUSize >> param->uiMaxCUDepth),
+        "Minimum CU height must be greater than minimum transform size.");
+    CONFIRM(param->uiQuadtreeTUMaxDepthInter < 1,
+        "QuadtreeTUMaxDepthInter must be greater than or equal to 1");
+    CONFIRM(param->uiMaxCUSize < (1u << (param->uiQuadtreeTULog2MinSize + param->uiQuadtreeTUMaxDepthInter - 1)),
+        "QuadtreeTUMaxDepthInter must be less than or equal to the difference between log2(maxCUSize) and QuadtreeTULog2MinSize plus 1");
+    CONFIRM(param->uiQuadtreeTUMaxDepthIntra < 1,
+        "QuadtreeTUMaxDepthIntra must be greater than or equal to 1");
+    CONFIRM(param->uiMaxCUSize < (1u << (param->uiQuadtreeTULog2MinSize + param->uiQuadtreeTUMaxDepthIntra - 1)),
+        "QuadtreeTUMaxDepthInter must be less than or equal to the difference between log2(maxCUSize) and QuadtreeTULog2MinSize plus 1");
+
+    CONFIRM(param->maxNumMergeCand < 1, "MaxNumMergeCand must be 1 or greater.");
+    CONFIRM(param->maxNumMergeCand > 5, "MaxNumMergeCand must be 5 or smaller.");
+
+    CONFIRM(param->bUseAdaptQpSelect && param->iQP < 0,
+        "AdaptiveQpSelection must be disabled when QP < 0.");
+    CONFIRM(param->bUseAdaptQpSelect && (param->cbQpOffset != 0 || param->crQpOffset != 0),
+        "AdaptiveQpSelection must be disabled when ChromaQpOffset is not equal to 0.");
+
+    //TODO:ChromaFmt assumes 4:2:0 below
+    CONFIRM(param->iSourceWidth  % TComSPS::getWinUnitX(CHROMA_420) != 0,
+        "Picture width must be an integer multiple of the specified chroma subsampling");
+    CONFIRM(param->iSourceHeight % TComSPS::getWinUnitY(CHROMA_420) != 0,
+        "Picture height must be an integer multiple of the specified chroma subsampling");
+
+    // max CU size should be power of 2
+    uint32_t ui = param->uiMaxCUSize;
+    while (ui)
+    {
+        ui >>= 1;
+        if ((ui & 1) == 1)
+            CONFIRM(ui != 1, "Width should be 2^n");
+    }
+
+    CONFIRM(param->iWaveFrontSynchro < 0, "WaveFrontSynchro cannot be negative");
+
+    CONFIRM(param->log2ParallelMergeLevel < 2, "Log2ParallelMergeLevel should be larger than or equal to 2");
+
+    return check_failed;
+}
+
+void x265_set_globals(x265_param_t *param, uint32_t inputBitDepth)
+{
+    // set max CU width & height
+    g_uiMaxCUWidth  = param->uiMaxCUSize;
+    g_uiMaxCUHeight = param->uiMaxCUSize;
+
+    // compute actual CU depth with respect to config depth and max transform size
+    g_uiAddCUDepth  = 0;
+    while ((param->uiMaxCUSize >> param->uiMaxCUDepth) > (1u << (param->uiQuadtreeTULog2MinSize + g_uiAddCUDepth)))
+    {
+        g_uiAddCUDepth++;
+    }
+
+    param->uiMaxCUDepth += g_uiAddCUDepth;
+    g_uiAddCUDepth++;
+    g_uiMaxCUDepth = param->uiMaxCUDepth;
+
+    // set internal bit-depth and constants
+#if HIGH_BIT_DEPTH
+    g_bitDepthY = param->internalBitDepth;
+    g_bitDepthC = param->internalBitDepth;
+#else
+    g_bitDepthY = g_bitDepthC = 8;
+    g_uiPCMBitDepthLuma = g_uiPCMBitDepthChroma = 8;
+#endif
+
+    g_uiPCMBitDepthLuma = inputBitDepth;
+    g_uiPCMBitDepthChroma = inputBitDepth;
+}
+
+void x265_print_params(x265_param_t *param)
+{
+    printf("x265: Format                       : %dx%d %dHz\n", param->iSourceWidth, param->iSourceHeight, param->iFrameRate);
+#if HIGH_BIT_DEPTH
+    printf("x265: Internal bit depth           : %d\n", param->internalBitDepth);
+#endif
+    printf("x265: CU size / depth              : %d / %d\n", param->uiMaxCUSize, param->uiMaxCUDepth);
+    printf("x265: RQT trans. size (min / max)  : %d / %d\n", 1 << param->uiQuadtreeTULog2MinSize, 1 << param->uiQuadtreeTULog2MaxSize);
+    printf("x265: Max RQT depth inter / intra  : %d / %d\n", param->uiQuadtreeTUMaxDepthInter, param->uiQuadtreeTUMaxDepthIntra);
+
+    printf("x265: Motion search / range        : %s / %d\n", x265_motion_est_names[param->searchMethod], param->iSearchRange );
+    printf("x265: Max Num Merge Candidates     : %d ", param->maxNumMergeCand);
+    printf("PME:%d ", param->log2ParallelMergeLevel);
+    printf("TMVPMode:%d\n", param->TMVPModeId);
+    printf("x265: Intra period                 : %d\n", param->iIntraPeriod);
+    if (param->iWaveFrontSynchro)
+    {
+        printf("x265: WaveFrontSubstreams          : %d\n", (param->iSourceHeight + param->uiMaxCUSize - 1) / param->uiMaxCUSize);
+    }
+    printf("x265: QP                           : %d\n", param->iQP);
+    if (param->iMaxCuDQPDepth)
+    {
+        printf("x265: Max dQP signaling depth      : %d\n", param->iMaxCuDQPDepth);
+    }
+    if (param->cbQpOffset || param->crQpOffset)
+    {
+        printf("x265: Cb/Cr QP Offset              : %d / %d\n", param->cbQpOffset, param->crQpOffset);
+    }
+    if (param->bUseAdaptiveQP)
+    {
+        printf("x265: QP adaptation                : %d (range=%d)\n", param->bUseAdaptiveQP, param->iQPAdaptationRange);
+    }
+    if (param->rdPenalty)
+    {
+        printf("x265: RDpenalty                    : %d\n", param->rdPenalty);
+    }
+    printf("x265: enabled coding tools: ");
+#define TOOLOPT(FLAG, STR) if (FLAG) printf("%s ", STR)
+    TOOLOPT(param->useRDOQ, "rdq");
+    TOOLOPT(param->useRDOQTS, "rdqts");
+    TOOLOPT(param->useFastDecisionForMerge, "fdm");
+    TOOLOPT(param->bUseCbfFastMode, "cfm");
+    TOOLOPT(param->useEarlySkipDetection, "esd");
+    TOOLOPT(param->useTransformSkip, "tskip");
+    TOOLOPT(param->useTransformSkipFast, "tskip-fast");
+    if (param->bUseSAO)
+    {
+        TOOLOPT(param->bUseSAO, "sao");
+        TOOLOPT(param->saoLcuBasedOptimization, "sao-lcu");
+    }
+    TOOLOPT(param->useWeightedPred, "weightp");
+    TOOLOPT(param->useWeightedBiPred, "weightbp");
+    TOOLOPT(param->bUseAdaptQpSelect, "aq");
+    TOOLOPT(param->signHideFlag, "sign-hide");
+    TOOLOPT(param->bUseConstrainedIntraPred, "cip");
+    printf("\n\n");
+    fflush(stdout);
+}
---
+++ source/x265.cpp
@@ -0,0 +1,384 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#if _MSC_VER
+#pragma warning(disable: 4127) // conditional expression is constant, yes I know
+#endif
+
+#include "input/input.h"
+#include "output/output.h"
+#include "threadpool.h"
+#include "primitives.h"
+#include "common.h"
+#include "PPA/ppa.h"
+#include "x265.h"
+
+#if HAVE_VLD
+/* Visual Leak Detector */
+#include <vld.h>
+#endif
+
+#include <getopt.h>
+#include <assert.h>
+#include <string.h>
+#include <time.h>
+#include <list>
+#include <ostream>
+#include <fstream>
+
+using namespace x265;
+using namespace std;
+
+struct CLIOptions
+{
+    x265::Input*  input;
+    x265::Output* recon;
+    x265::ThreadPool *threadPool;
+    fstream bitstreamFile;
+
+    uint32_t inputBitDepth;             ///< bit-depth of input file
+    uint32_t outputBitDepth;            ///< bit-depth of output reconstructed images file
+    uint32_t frameSkip;                 ///< number of frames to skip from the beginning
+    uint32_t framesToBeEncoded;         ///< number of frames to encode
+
+    uint32_t essentialBytes;            ///< total essential NAL bytes written to bitstream
+    uint32_t totalBytes;                ///< total bytes written to bitstream
+
+    CLIOptions()
+    {
+        input = NULL;
+        recon = NULL;
+        threadPool = NULL;
+        inputBitDepth = outputBitDepth = 8;
+        framesToBeEncoded = frameSkip = 0;
+        essentialBytes = 0;
+        totalBytes = 0;
+    }
+
+    void destroy()
+    {
+        if (threadPool)
+            threadPool->Release();
+        threadPool = NULL;
+        if (input)
+            input->release();
+        input = NULL;
+        if (recon)
+            recon->release();
+        recon = NULL;
+    }
+
+    void rateStatsAccum(NalUnitType nalUnitType, uint32_t annexBsize)
+    {
+        switch (nalUnitType)
+        {
+        case NAL_UNIT_CODED_SLICE_TRAIL_R:
+        case NAL_UNIT_CODED_SLICE_TRAIL_N:
+        case NAL_UNIT_CODED_SLICE_TLA_R:
+        case NAL_UNIT_CODED_SLICE_TSA_N:
+        case NAL_UNIT_CODED_SLICE_STSA_R:
+        case NAL_UNIT_CODED_SLICE_STSA_N:
+        case NAL_UNIT_CODED_SLICE_BLA_W_LP:
+        case NAL_UNIT_CODED_SLICE_BLA_W_RADL:
+        case NAL_UNIT_CODED_SLICE_BLA_N_LP:
+        case NAL_UNIT_CODED_SLICE_IDR_W_RADL:
+        case NAL_UNIT_CODED_SLICE_IDR_N_LP:
+        case NAL_UNIT_CODED_SLICE_CRA:
+        case NAL_UNIT_CODED_SLICE_RADL_N:
+        case NAL_UNIT_CODED_SLICE_RADL_R:
+        case NAL_UNIT_CODED_SLICE_RASL_N:
+        case NAL_UNIT_CODED_SLICE_RASL_R:
+        case NAL_UNIT_VPS:
+        case NAL_UNIT_SPS:
+        case NAL_UNIT_PPS:
+            essentialBytes += annexBsize;
+            break;
+        default:
+            break;
+        }
+
+        totalBytes += annexBsize;
+    }
+
+    void writeNALs(const x265_nal_t* nal, int nalcount)
+    {
+        PPAStartCpuEventFunc(bitstream_write);
+        for (int i = 0; i < nalcount; i++)
+        {
+            bitstreamFile.write((const char*) nal->p_payload, nal->i_payload);
+            rateStatsAccum((NalUnitType) nal->i_type, nal->i_payload);
+            nal++;
+        }
+        PPAStopCpuEventFunc(bitstream_write);
+    }
+};
+
+static void print_version()
+{
+#define XSTR(x) STR(x)
+#define STR(x) #x
+    fprintf(stdout, "x265: HEVC encoder version %s\n", XSTR(X265_VERSION));
+    fprintf(stdout, "x265: build info ");
+    fprintf(stdout, NVM_ONOS);
+    fprintf(stdout, NVM_COMPILEDBY);
+    fprintf(stdout, NVM_BITS);
+#if HIGH_BIT_DEPTH
+    fprintf(stdout, "16bpp");
+#else
+    fprintf(stdout, "8bpp");
+#endif
+    fprintf(stdout, "\n");
+}
+
+static void do_help()
+{
+    print_version();
+    printf("Options:\n");
+
+#define HELP(message) printf("\n%s\n", message);
+#define OPT(longname, var, argreq, flag, helptext)\
+    if (flag) printf("-%c/", flag); else printf("   ");\
+    printf("--%-20s\t%s\n", longname, helptext);
+#define STROPT OPT
+#include "x265opts.h"
+#undef OPT
+#undef STROPT
+#undef HELP
+#define HELP(message)
+
+    exit(0);
+}
+
+static const char short_options[] = "h:o:f:r:i:s:d:q:w:V:";
+static struct option long_options[] =
+{
+#define OPT(longname, var, argreq, flag, helptext) { longname, argreq, NULL, flag },
+#define STROPT OPT
+#include "x265opts.h"
+#undef OPT
+#undef STROPT
+};
+
+bool parse(int argc, char **argv, x265_param_t* param, CLIOptions* cliopt)
+{
+    int help = 0;
+    int cpuid = 0;
+    int threadcount = 0;
+    const char *inputfn = NULL, *reconfn = NULL, *bitstreamfn = NULL;
+
+    x265_param_default(param);
+
+    for( optind = 0;; )
+    {
+        int long_options_index = -1;
+        int c = getopt_long(argc, argv, short_options, long_options, &long_options_index);
+        if (c == -1)
+        {
+            break;
+        }
+
+        switch (c)
+        {
+        case 'h':
+            do_help();
+        case 'V':
+            print_version();
+            exit(0);
+        default:
+            if (long_options_index < 0 && c > 0)
+            {
+                for (size_t i = 0; i < sizeof(long_options)/sizeof(long_options[0]); i++)
+                    if (long_options[i].val == c)
+                    {
+                        long_options_index = (int)i;
+                        break;
+                    }
+            }
+            if (long_options_index < 0)
+                printf("x265: short option '%x' unrecognized\n", c);
+#define STROPT(longname, var, argreq, flag, helptext)\
+            else if (!strcmp(long_options[long_options_index].name, longname))\
+                (var) = optarg;
+#define OPT(longname, var, argreq, flag, helptext)\
+            else if (!strcmp(long_options[long_options_index].name, longname))\
+                (var) = (flag == no_argument) ? (strncmp(longname, "no-", 3) ? 1 : 0) : atoi(optarg);
+#include "x265opts.h"
+#undef OPT
+#undef STROPT
+        }
+    }
+
+    if (argc <= 1 || help)
+        do_help();
+
+    x265::SetupPrimitives(cpuid);
+    cliopt->threadPool = x265::ThreadPool::AllocThreadPool(threadcount);
+
+    if (optind < argc)
+        inputfn = argv[optind];
+    if (inputfn == NULL)
+    {
+        printf("x265: no input file specified, try -V for help\n");
+        return true;
+    }
+    cliopt->input = x265::Input::Open(inputfn);
+    if (!cliopt->input || cliopt->input->isFail())
+    {
+        printf("x265: unable to open input file <%s>\n", inputfn);
+        return true;
+    }
+    if (cliopt->input->getWidth())
+    {
+        /* parse the width, height, frame rate from the y4m file */
+        param->iSourceWidth = cliopt->input->getWidth();
+        param->iSourceHeight = cliopt->input->getHeight();
+        param->iFrameRate = (int)cliopt->input->getRate();
+        cliopt->inputBitDepth = 8;
+    }
+    else
+    {
+        cliopt->input->setDimensions(param->iSourceWidth, param->iSourceHeight);
+        cliopt->input->setBitDepth(cliopt->inputBitDepth);
+    }
+    assert(param->iSourceHeight && param->iSourceWidth);
+
+    /* rules for input, output and internal bitdepths as per help text */
+    if (!param->internalBitDepth) { param->internalBitDepth = cliopt->inputBitDepth; }
+    if (!cliopt->outputBitDepth) { cliopt->outputBitDepth = param->internalBitDepth; }
+
+    uint32_t numRemainingFrames = (uint32_t)cliopt->input->guessFrameCount();
+
+    if (cliopt->frameSkip)
+    {
+        cliopt->input->skipFrames(cliopt->frameSkip);
+    }
+
+    cliopt->framesToBeEncoded = cliopt->framesToBeEncoded ? min(cliopt->framesToBeEncoded, numRemainingFrames) : numRemainingFrames;
+
+    printf("x265: Input File                   : %s (%u - %d of %d total frames)\n", inputfn,
+        cliopt->frameSkip, cliopt->frameSkip + cliopt->framesToBeEncoded - 1, numRemainingFrames);
+
+    if (reconfn)
+    {
+        cliopt->recon = x265::Output::Open(reconfn, param->iSourceWidth, param->iSourceHeight, cliopt->outputBitDepth, param->iFrameRate);
+        if (cliopt->recon->isFail())
+        {
+            printf("x265: unable to write reconstruction file\n");
+            cliopt->recon->release();
+            cliopt->recon = 0;
+        }
+    }
+
+#if !HIGH_BIT_DEPTH
+    if (cliopt->inputBitDepth != 8 || cliopt->outputBitDepth != 8 || param->internalBitDepth != 8)
+    {
+        printf("x265: not compiled for bit depths greater than 8\n");
+        return true;
+    }
+#endif
+
+    cliopt->bitstreamFile.open(bitstreamfn, fstream::binary | fstream::out);
+    if (!cliopt->bitstreamFile)
+    {
+        fprintf(stderr, "x265: failed to open bitstream file <%s> for writing\n", bitstreamfn);
+        return true;
+    }
+
+    return false;
+}
+
+int main(int argc, char **argv)
+{
+#if HAVE_VLD
+    VLDSetReportOptions(VLD_OPT_REPORT_TO_DEBUGGER, NULL);
+#endif
+    PPA_INIT();
+
+    x265_param_t param;
+    CLIOptions   cliopt;
+
+    // TODO: needs proper logging file handle with log levels, etc
+    if (parse(argc, argv, &param, &cliopt))
+        exit(1);
+
+    x265_set_globals(&param, cliopt.inputBitDepth);
+
+    if (x265_check_params(&param))
+        exit(1);
+
+    x265_print_params(&param);
+
+    x265_t *encoder = x265_encoder_open(&param);
+    if (!encoder)
+    {
+        fprintf(stderr, "x265: failed to open encoder\n");
+        exit(1);
+    }
+
+    x265_picture_t pic_orig, pic_recon;
+    x265_picture_t *pic_in = &pic_orig;
+    x265_picture_t *pic_out = cliopt.recon ? &pic_recon : 0;
+    x265_nal_t *p_nal;
+    int nal;
+
+    clock_t start = clock();
+
+    // main encoder loop
+    uint32_t frameCount = 0;
+    while (pic_in)
+    {
+        // read input YUV file
+        if (frameCount < cliopt.framesToBeEncoded && cliopt.input->readPicture(pic_orig))
+            frameCount++;
+        else
+            pic_in = NULL;
+
+        int iNumEncoded = x265_encoder_encode(encoder, &p_nal, &nal, pic_in, pic_out);
+        if (iNumEncoded && pic_out)
+            cliopt.recon->writePicture(pic_recon);
+        if (nal)
+            cliopt.writeNALs(p_nal, nal);
+    }
+
+    /* Flush the encoder */
+    while (x265_encoder_encode(encoder, &p_nal, &nal, NULL, pic_out))
+    {
+        if (pic_out)
+            cliopt.recon->writePicture(pic_recon);
+        if (nal)
+            cliopt.writeNALs(p_nal, nal);
+    }
+
+    x265_encoder_close(encoder);
+    cliopt.bitstreamFile.close();
+
+    double elapsed = (double)(clock() - start) / CLOCKS_PER_SEC;
+    double vidtime = (double)frameCount / param.iFrameRate;
+    printf("Bytes written to file: %u (%.3f kbps) in %3.3f sec\n",
+        cliopt.totalBytes, 0.008 * cliopt.totalBytes / vidtime, elapsed);
+
+#if HAVE_VLD
+    assert(VLDReportLeaks() == 0);
+#endif
+    return 0;
+}
