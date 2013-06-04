a9f02ed1ffd762ea27eb4bc359ecb03c8a15b190
ggopu <ggopu@localhost>
2013-06-03 19:21:09 -0700

    * m_pcRdCost->getBits() replaced with BitCost.mvcost * Fixed other code blocks calling non-primitive SAD functions (crashes at sad8 - for load_a whereas works with just load) * Primitives for SSE  functions

---
+++ source/encoder/bitcost.cpp
@@ -0,0 +1,122 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#include "bitcost.h"
+#include <stdint.h>
+#include <algorithm>
+#include <math.h>
+
+using namespace x265;
+
+void BitCost::setQP(unsigned int qp, double lambda)
+{
+    if (!s_costs[qp])
+    {
+        ScopedLock s(s_costCalcLock);
+
+        // Now that we have acquired the lock, check again if another thread calculated
+        // this row while we were blocked
+        if (!s_costs[qp])
+        {
+            CalculateLogs();
+            s_costs[qp] = new uint16_t[2 * BC_MAX_MV] + BC_MAX_MV;
+            uint16_t *c = s_costs[qp];
+            const uint16_t max16 = (1 << 16)-1;
+
+#if X264_APPROACH
+            // estimate same cost for negative and positive MVD
+            for (int i = 0; i < BC_MAX_MV; i++)
+                c[i] = c[-i] = std::min<uint16_t>(max16, (uint16_t)(logs[i] * lambda + 0.5));
+#else
+            // penalize negative MVD by one bit
+            c[0] = (uint16_t)lambda;
+            for (int i = 1; i < BC_MAX_MV; i++)
+            {
+                c[i]  = std::min<uint16_t>(max16, (uint16_t)(s_bitsizes[i<<1] * lambda + 0.5));
+                c[-i] = std::min<uint16_t>(max16, (uint16_t)(s_bitsizes[(i<<1)+1] * lambda + 0.5));
+            }
+#endif
+        }
+    }
+
+    m_cost = s_costs[qp];
+}
+
+/***
+ * Class static data and methods
+ */
+
+uint16_t *BitCost::s_costs[BC_MAX_QP];
+
+uint16_t *BitCost::s_bitsizes;
+
+Lock BitCost::s_costCalcLock;
+
+void BitCost::CalculateLogs()
+{
+    if (!s_bitsizes)
+    {
+        s_bitsizes = new uint16_t[2*BC_MAX_MV + 1];
+        s_bitsizes[0] = 1;
+#if X264_APPROACH
+        float log2_2 = (float)(2.0/log(2.0));  // 2 x 1/log(2)
+        for( int i = 1; i <= 2*BC_MAX_MV; i++ )
+            s_bitsizes[i] = (uint16_t)(log((float)(i+1)) * log2_2 + 1.718f);
+#else
+        s_bitsizes[1] = 1;
+        for( int i = 2; i <= 2*BC_MAX_MV; i++ )
+        {
+            // TODO: should be possible to replace this loop with a log() and
+            //       match the outputs.  Not a priority since this only runs once.
+            int temp = i;
+            uint16_t len = 1;
+            while (1 != temp)
+            {
+                temp >>= 1;
+                len += 2;
+            }
+
+            s_bitsizes[i] = len;
+        }
+#endif
+    }
+}
+
+void BitCost::destroy()
+{
+    for (int i = 0; i < BC_MAX_QP; i++)
+    {
+        if (s_costs[i])
+        {
+            delete [] (s_costs[i] - BC_MAX_MV);
+
+            s_costs[i] = 0;
+        }
+    }
+    if (s_bitsizes)
+    {
+        delete [] s_bitsizes;
+
+        s_bitsizes = 0;
+    }
+}
---
+++ source/test/testbench.cpp
@@ -0,0 +1,157 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Gopu Govindaswamy <gopu@govindaswamy.org>
+ *          Mandar Gurav <mandar@multicorewareinc.com>
+ *          Mahesh Pittala <mahesh@multicorewareinc.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#include "primitives.h"
+#include "pixelharness.h"
+#include "mbdstharness.h"
+#include "ipfilterharness.h"
+#include "intrapredharness.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <malloc.h>
+
+using namespace x265;
+
+#ifdef __MINGW32__
+#define _aligned_malloc __mingw_aligned_malloc
+#define _aligned_free  __mingw_aligned_free
+#endif
+
+void *TestHarness::alignedMalloc(size_t size, int count, int alignment)
+{
+#if _WIN32
+    return _aligned_malloc(count * size, alignment);
+#else
+    void *ptr;
+    if (posix_memalign((void**)&ptr, alignment, count * size) == 0)
+        return ptr;
+    else
+        return NULL;
+#endif
+}
+
+void TestHarness::alignedFree(void *ptr)
+{
+#if _WIN32
+    _aligned_free(ptr);
+#else
+    free(ptr);
+#endif
+}
+
+extern int instrset_detect();
+
+int main(int argc, char *argv[])
+{
+    int cpuid = instrset_detect(); // Detect supported instruction set
+
+    for (int i = 1; i < argc - 1; i += 2)
+    {
+        if (!strcmp(argv[i], "--cpuid"))
+        {
+            cpuid = atoi(argv[i + 1]);
+        }
+    }
+
+    int seed = (int)time(NULL);
+    const char *bpp[] = { "8bpp", "16bpp" };
+    printf("Using random seed %X %s\n", seed, bpp[HIGH_BIT_DEPTH]);
+    srand(seed);
+
+    PixelHarness  HPixel;
+    MBDstHarness  HMBDist;
+    IPFilterHarness HIPFilter;
+    IntraPredHarness HIPred;
+
+    // To disable classes of tests, simply comment them out in this list
+    TestHarness *harness[] =
+    {
+        &HPixel,
+        &HMBDist,
+        &HIPFilter,
+        &HIPred
+    };
+
+    EncoderPrimitives cprim;
+    memset(&cprim, 0, sizeof(EncoderPrimitives));
+    Setup_C_Primitives(cprim);
+
+    for (int i = 1; i <= cpuid; i++)
+    {
+#if ENABLE_VECTOR_PRIMITIVES
+        EncoderPrimitives vecprim;
+        memset(&vecprim, 0, sizeof(vecprim));
+        Setup_Vector_Primitives(vecprim, i);
+        printf("Testing vector class primitives: CPUID %d\n", i);
+        for (size_t h = 0; h < sizeof(harness) / sizeof(TestHarness*); h++)
+        {
+            if (!harness[h]->testCorrectness(cprim, vecprim))
+            {
+                fprintf(stderr, "\nx265: vector primitive has failed. Go and fix that Right Now!\n");
+                return -1;
+            }
+        }
+#endif // if ENABLE_VECTOR_PRIMITIVES
+
+#if ENABLE_ASM_PRIMITIVES
+        EncoderPrimitives asmprim;
+        memset(&asmprim, 0, sizeof(asmprim));
+        Setup_Assembly_Primitives(asmprim, i);
+        printf("Testing assembly primitives: CPUID %d\n", i);
+        for (size_t h = 0; h < sizeof(harness) / sizeof(TestHarness*); h++)
+        {
+            if (!harness[h]->testCorrectness(cprim, vecprim))
+            {
+                fprintf(stderr, "\nx265: ASM primitive has failed. Go and fix that Right Now!\n");
+                return -1;
+            }
+        }
+#endif // if ENABLE_ASM_PRIMITIVES
+    }
+
+    /******************* Cycle count for all primitives **********************/
+
+    EncoderPrimitives optprim;
+    memset(&optprim, 0, sizeof(optprim));
+#if ENABLE_VECTOR_PRIMITIVES
+    Setup_Vector_Primitives(optprim, cpuid);
+#endif
+#if ENABLE_ASM_PRIMITIVES
+    Setup_Assembly_Primitives(optprim, cpuid);
+#endif
+
+    fprintf(stderr, "\nTest performance improvement with full optimizations\n");
+
+    for (size_t h = 0; h < sizeof(harness) / sizeof(TestHarness*); h++)
+    {
+        harness[h]->measureSpeed(cprim, optprim);
+    }
+
+    printf("\n");
+    return 0;
+}
