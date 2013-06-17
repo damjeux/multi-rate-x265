175e7cf3d72228c90faf1f180c49cea45ef6c2ae
Deepthi Devaki <Deepthi Devaki@localhost>
2013-06-17 10:32:29 +0530

    Used saturated compress to eliminate min()

---
+++ source/encoder/wavefront.cpp
@@ -0,0 +1,272 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Chung Shin Yee <shinyee@multicorewareinc.com>
+ *          Min Chen <chenm003@163.com>
+ *          Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#include "TLibEncoder/TEncTop.h"
+#include "PPA/ppa.h"
+#include "wavefront.h"
+
+using namespace x265;
+
+void CTURow::create(TEncTop* top)
+{
+    m_cRDGoOnSbacCoder.init(&m_cRDGoOnBinCodersCABAC);
+    m_cSbacCoder.init(&m_cBinCoderCABAC);
+    m_cSearch.init(top, &m_cRdCost);
+
+    m_cCuEncoder.create((UChar)g_uiMaxCUDepth, g_uiMaxCUWidth, g_uiMaxCUHeight);
+    m_cCuEncoder.init(top);
+
+    if (top->getUseAdaptiveQP())
+    {
+        m_cTrQuant.initSliceQpDelta();
+    }
+    m_cTrQuant.init(1 << top->getQuadtreeTULog2MaxSize(), top->getUseRDOQ(), top->getUseRDOQTS(), true,
+                    top->getUseTransformSkipFast(), top->getUseAdaptQpSelect());
+
+    m_pppcRDSbacCoders = new TEncSbac * *[g_uiMaxCUDepth + 1];
+    m_pppcBinCodersCABAC = new TEncBinCABACCounter * *[g_uiMaxCUDepth + 1];
+
+    for (UInt iDepth = 0; iDepth < g_uiMaxCUDepth + 1; iDepth++)
+    {
+        m_pppcRDSbacCoders[iDepth]  = new TEncSbac*[CI_NUM];
+        m_pppcBinCodersCABAC[iDepth] = new TEncBinCABACCounter*[CI_NUM];
+
+        for (Int iCIIdx = 0; iCIIdx < CI_NUM; iCIIdx++)
+        {
+            m_pppcRDSbacCoders[iDepth][iCIIdx] = new TEncSbac;
+            m_pppcBinCodersCABAC[iDepth][iCIIdx] = new TEncBinCABACCounter;
+            m_pppcRDSbacCoders[iDepth][iCIIdx]->init(m_pppcBinCodersCABAC[iDepth][iCIIdx]);
+        }
+    }
+
+    m_cCuEncoder.set_pcRdCost(&m_cRdCost);
+    m_cCuEncoder.set_pppcRDSbacCoder(m_pppcRDSbacCoders);
+    m_cCuEncoder.set_pcEntropyCoder(&m_cEntropyCoder);
+    m_cCuEncoder.set_pcPredSearch(&m_cSearch);
+    m_cCuEncoder.set_pcTrQuant(&m_cTrQuant);
+    m_cCuEncoder.set_pcRdCost(&m_cRdCost);
+}
+
+void CTURow::processCU(TComDataCU *pcCU, TComSlice *pcSlice, TEncSbac *pcBufferSBac, bool bSaveSBac)
+{
+    TEncBinCABAC* pcRDSbacCoder = (TEncBinCABAC*)m_pppcRDSbacCoders[0][CI_CURR_BEST]->getEncBinIf();
+    pcRDSbacCoder->setBinCountingEnableFlag(false);
+    pcRDSbacCoder->setBinsCoded(0);
+
+    if (pcBufferSBac)
+    {
+        // Load SBAC coder context from previous row.
+        m_pppcRDSbacCoders[0][CI_CURR_BEST]->loadContexts(pcBufferSBac);
+    }
+
+    m_cEntropyCoder.setEntropyCoder(&m_cRDGoOnSbacCoder, pcSlice);
+    m_cEntropyCoder.setBitstream(&m_cBitCounter);
+    ((TEncBinCABAC*)m_cRDGoOnSbacCoder.getEncBinIf())->setBinCountingEnableFlag(true);
+    m_cCuEncoder.set_pcRDGoOnSbacCoder(&m_cRDGoOnSbacCoder);
+
+    m_cCuEncoder.compressCU(pcCU); // Does all the CU analysis
+
+    // restore entropy coder to an initial state
+    m_cEntropyCoder.setEntropyCoder(m_pppcRDSbacCoders[0][CI_CURR_BEST], pcSlice);
+    m_cEntropyCoder.setBitstream(&m_cBitCounter);
+    m_cCuEncoder.setBitCounter(&m_cBitCounter);
+    pcRDSbacCoder->setBinCountingEnableFlag(true);
+    m_cBitCounter.resetBits();
+    pcRDSbacCoder->setBinsCoded(0);
+
+    m_cCuEncoder.encodeCU(pcCU);  // Count bits
+
+    pcRDSbacCoder->setBinCountingEnableFlag(false);
+    
+    if (bSaveSBac)
+    {
+        // Save CABAC state for next row
+        m_cBufferSbacCoder.loadContexts(m_pppcRDSbacCoders[0][CI_CURR_BEST]);
+    }
+}
+
+void CTURow::destroy()
+{
+    for (UInt iDepth = 0; iDepth < g_uiMaxCUDepth + 1; iDepth++)
+    {
+        for (Int iCIIdx = 0; iCIIdx < CI_NUM; iCIIdx++)
+        {
+            delete m_pppcRDSbacCoders[iDepth][iCIIdx];
+            delete m_pppcBinCodersCABAC[iDepth][iCIIdx];
+        }
+    }
+
+    for (UInt iDepth = 0; iDepth < g_uiMaxCUDepth + 1; iDepth++)
+    {
+        delete [] m_pppcRDSbacCoders[iDepth];
+        delete [] m_pppcBinCodersCABAC[iDepth];
+    }
+
+    delete[] m_pppcRDSbacCoders;
+    delete[] m_pppcBinCodersCABAC;
+    m_cCuEncoder.destroy();
+}
+
+EncodeFrame::EncodeFrame(ThreadPool* pool)
+    : QueueFrame(pool)
+    , m_pcCfg(NULL)
+    , m_pcSlice(NULL)
+    , m_pic(NULL)
+    , m_rows(NULL)
+{}
+
+void EncodeFrame::destroy()
+{
+    JobProvider::Flush();  // ensure no worker threads are using this frame
+
+    if (m_rows)
+    {
+        for (int i = 0; i < m_nrows; ++i)
+        {
+            m_rows[i].destroy();
+        }
+
+        delete[] m_rows;
+    }
+
+    m_cSliceEncoder.destroy();
+    if (m_pcCfg->getUseSAO())
+    {
+        m_cEncSAO.destroy();
+        m_cEncSAO.destroyEncBuffer();
+    }
+    m_cLoopFilter.destroy();
+}
+
+void EncodeFrame::init(TEncTop *top)
+{
+    m_pcCfg = top;
+    m_nrows = top->getNumSubstreams();
+    m_enableWpp = top->getWaveFrontsynchro() ? true : false;
+
+    m_cSliceEncoder.init(top);
+    m_cSliceEncoder.create(top->getSourceWidth(), top->getSourceHeight(), g_uiMaxCUWidth, g_uiMaxCUHeight, (UChar) g_uiMaxCUDepth);
+    if (top->getUseSAO())
+    {
+        m_cEncSAO.setSaoLcuBoundary(top->getSaoLcuBoundary());
+        m_cEncSAO.setSaoLcuBasedOptimization(top->getSaoLcuBasedOptimization());
+        m_cEncSAO.setMaxNumOffsetsPerPic(top->getMaxNumOffsetsPerPic());
+        m_cEncSAO.create(top->getSourceWidth(), top->getSourceHeight(), g_uiMaxCUWidth, g_uiMaxCUHeight);
+        m_cEncSAO.createEncBuffer();
+    }
+    m_cLoopFilter.create(g_uiMaxCUDepth);
+
+    m_rows = new CTURow[m_nrows];
+    for (int i = 0; i < m_nrows; ++i)
+    {
+        m_rows[i].create(top);
+    }
+
+    if (!QueueFrame::InitJobQueue(m_nrows))
+    {
+        assert(!"Unable to initialize job queue.");
+        throw;
+    }
+}
+
+void EncodeFrame::Encode(TComPic *pic, TComSlice *pcSlice)
+{
+    m_pic = pic;
+    m_pcSlice = pcSlice;
+
+    // reset entropy coders
+    m_cSbacCoder.init(&m_cBinCoderCABAC);
+    for (int i = 0; i < this->m_nrows; i++)
+    {
+        m_rows[i].init();
+        m_rows[i].m_cEntropyCoder.setEntropyCoder(&m_cSbacCoder, m_pcSlice);
+        m_rows[i].m_cEntropyCoder.resetEntropy();
+        m_rows[i].m_pppcRDSbacCoders[0][CI_CURR_BEST]->load(&m_cSbacCoder);
+    }
+
+    QueueFrame::Enqueue();
+
+    // Enqueue first row, then block until worker threads complete the frame
+    QueueFrame::EnqueueRow(0);
+    m_completionEvent.Wait();
+
+    QueueFrame::Dequeue();
+}
+
+void EncodeFrame::ProcessRow(int irow)
+{
+    PPAScopeEvent(Thread_ProcessRow);
+
+    // Called by worker threads
+    CTURow& curRow  = m_rows[irow];
+    CTURow& codeRow = m_rows[m_enableWpp ? irow : 0];
+
+    const uint32_t numCols = m_pic->getPicSym()->getFrameWidthInCU();
+    const uint32_t lineStartCUAddr = irow * numCols;
+    for (UInt uiCol = curRow.m_curCol; uiCol < numCols; uiCol++)
+    {
+        const uint32_t uiCUAddr = lineStartCUAddr + uiCol;
+        TComDataCU* pcCU = m_pic->getCU(uiCUAddr);
+        pcCU->initCU(m_pic, uiCUAddr);
+
+        codeRow.m_cEntropyCoder.setEntropyCoder(&m_cSbacCoder, m_pcSlice);
+        codeRow.m_cEntropyCoder.resetEntropy();
+
+        TEncSbac *pcBufSBac = (m_enableWpp && uiCol == 0 && irow > 0) ? &m_rows[irow - 1].m_cBufferSbacCoder : NULL;
+        codeRow.processCU(pcCU, m_pcSlice, pcBufSBac, m_enableWpp && uiCol == 1);
+
+        // TODO: Keep atomic running totals for rate control?
+        // pcCU->getTotalBits();
+        // pcCU->getTotalCost();
+        // pcCU->getTotalDistortion();
+
+        // Completed CU processing
+        curRow.m_curCol++;
+
+        if (curRow.m_curCol >= 2 && irow < m_nrows - 1)
+        {
+            ScopedLock below(m_rows[irow + 1].m_lock);
+            if (m_rows[irow + 1].m_active == false &&
+                m_rows[irow + 1].m_curCol + 2 <= curRow.m_curCol)
+            {
+                m_rows[irow + 1].m_active = true;
+                QueueFrame::EnqueueRow(irow + 1);
+            }
+        }
+
+        ScopedLock self(curRow.m_lock);
+        if (irow > 0 && curRow.m_curCol < numCols - 1 && m_rows[irow - 1].m_curCol < curRow.m_curCol + 2)
+        {
+            curRow.m_active = false;
+            return;
+        }
+    }
+
+    // this row of CTUs has been encoded
+    if (irow == m_nrows - 1)
+    {
+        m_completionEvent.Trigger();
+    }
+}
---
+++ source/test/pixelharness.cpp
@@ -0,0 +1,609 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#include "pixelharness.h"
+#include "primitives.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+using namespace x265;
+
+// Initialize the Func Names for all the Pixel Comp
+static const char *FuncNames[NUM_PARTITIONS] =
+{
+    "  4x4", "  4x8", " 4x12", " 4x16", " 4x24", " 4x32", " 4x48", " 4x64",
+    "  8x4", "  8x8", " 8x12", " 8x16", " 8x24", " 8x32", " 8x48", " 8x64",
+    " 12x4", " 12x8", "12x12", "12x16", "12x24", "12x32", "12x48", "12x64",
+    " 16x4", " 16x8", "16x12", "16x16", "16x24", "16x32", "16x48", "16x64",
+    " 24x4", " 24x8", "24x12", "24x16", "24x24", "24x32", "24x48", "24x64",
+    " 32x4", " 32x8", "32x12", "32x16", "32x24", "32x32", "32x48", "32x64",
+    " 48x4", " 48x8", "48x12", "48x16", "48x24", "48x32", "48x48", "48x64",
+    " 64x4", " 64x8", "64x12", "64x16", "64x24", "64x32", "64x48", "64x64"
+};
+
+#if HIGH_BIT_DEPTH
+#define BIT_DEPTH 10
+#else
+#define BIT_DEPTH 8
+#endif
+
+#define PIXEL_MAX ((1 << BIT_DEPTH) - 1)
+
+PixelHarness::PixelHarness()
+{
+    pbuf1 = (pixel*)TestHarness::alignedMalloc(sizeof(pixel), 64 * 64 * 32, 32);
+    pbuf2 = (pixel*)TestHarness::alignedMalloc(sizeof(pixel), 64 * 64 * 32, 32);
+
+    sbuf1 = (short*)TestHarness::alignedMalloc(sizeof(short), 64 * 64 * 32, 32);
+    sbuf2 = (short*)TestHarness::alignedMalloc(sizeof(short), 64 * 64 * 32, 32);
+
+    if (!pbuf1 || !pbuf2)
+    {
+        fprintf(stderr, "malloc failed, unable to initiate tests!\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < 64 * 64 * 32; i++)
+    {
+        //Generate the Random Buffer for Testing
+        pbuf1[i] = rand() & PIXEL_MAX;
+        pbuf2[i] = rand() & PIXEL_MAX;
+
+        sbuf1[i] = rand() & PIXEL_MAX;
+        sbuf2[i] = rand() & PIXEL_MAX;
+    }
+}
+
+PixelHarness::~PixelHarness()
+{
+    TestHarness::alignedFree(pbuf1);
+    TestHarness::alignedFree(pbuf2);
+}
+
+#define INCR 16
+#define STRIDE 16
+
+bool PixelHarness::check_pixel_primitive(pixelcmp ref, pixelcmp opt)
+{
+    int j = 0;
+
+    for (int i = 0; i <= 100; i++)
+    {
+        int vres = opt(pbuf1, STRIDE, pbuf2 + j, STRIDE);
+        int cres = ref(pbuf1, STRIDE, pbuf2 + j, STRIDE);
+        if (vres != cres)
+            return false;
+
+        j += INCR;
+    }
+
+    return true;
+}
+
+bool PixelHarness::check_pixel_sp_primitive(pixelcmp_sp ref, pixelcmp_sp opt)
+{
+    int j = 0;
+
+    for (int i = 0; i <= 100; i++)
+    {
+        int vres = opt(sbuf1, STRIDE, pbuf2 + j, STRIDE);
+        int cres = ref(sbuf1, STRIDE, pbuf2 + j, STRIDE);
+        if (vres != cres)
+            return false;
+
+        j += INCR;
+    }
+
+    return true;
+}
+
+bool PixelHarness::check_pixel_ss_primitive(pixelcmp_ss ref, pixelcmp_ss opt)
+{
+    int j = 0;
+
+    for (int i = 0; i <= 100; i++)
+    {
+        int vres = opt(sbuf1, STRIDE, sbuf2 + j, STRIDE);
+        int cres = ref(sbuf1, STRIDE, sbuf2 + j, STRIDE);
+        if (vres != cres)
+            return false;
+
+        j += INCR;
+    }
+
+    return true;
+}
+
+bool PixelHarness::check_pixel_primitive_x3(pixelcmp_x3 ref, pixelcmp_x3 opt)
+{
+    int j = INCR;
+
+    ALIGN_VAR_16(int, cres[16]);
+    ALIGN_VAR_16(int, vres[16]);
+    for (int i = 0; i <= 100; i++)
+    {
+        opt(pbuf1, pbuf2 + j, pbuf2 + j + 1, pbuf2 + j - 1, FENC_STRIDE + 5, &vres[0]);
+        ref(pbuf1, pbuf2 + j, pbuf2 + j + 1, pbuf2 + j - 1, FENC_STRIDE + 5, &cres[0]);
+
+        if ((vres[0] != cres[0]) || ((vres[1] != cres[1])) || ((vres[2] != cres[2])))
+            return false;
+
+        j += INCR;
+    }
+
+    return true;
+}
+
+bool PixelHarness::check_pixel_primitive_x4(pixelcmp_x4 ref, pixelcmp_x4 opt)
+{
+    int j = INCR;
+
+    ALIGN_VAR_16(int, cres[16]);
+    ALIGN_VAR_16(int, vres[16]);
+    for (int i = 0; i <= 100; i++)
+    {
+        opt(pbuf1, pbuf2 + j, pbuf2 + j + 1, pbuf2 + j - 1, pbuf2 + j - INCR, FENC_STRIDE + 5, &vres[0]);
+        ref(pbuf1, pbuf2 + j, pbuf2 + j + 1, pbuf2 + j - 1, pbuf2 + j - INCR, FENC_STRIDE + 5, &cres[0]);
+
+        if ((vres[0] != cres[0]) || ((vres[1] != cres[1])) || ((vres[2] != cres[2])) || ((vres[3] != cres[3])))
+            return false;
+
+        j += INCR;
+    }
+
+    return true;
+}
+
+bool PixelHarness::check_block_copy(x265::blockcpy_p_p ref, x265::blockcpy_p_p opt)
+{
+    ALIGN_VAR_16(pixel, ref_dest[64 * 64]);
+    ALIGN_VAR_16(pixel, opt_dest[64 * 64]);
+    int bx = 64;
+    int by = 64;
+    int j = 0;
+    for (int i = 0; i <= 100; i++)
+    {
+        opt(bx, by, opt_dest, 64, pbuf2 + j, 128);
+        ref(bx, by, ref_dest, 64, pbuf2 + j, 128);
+
+        if (memcmp(ref_dest, opt_dest, 64 * 64 * sizeof(pixel)))
+            return false;
+
+        j += 4;
+        bx = 4 * ((rand() & 15) + 1);
+        by = 4 * ((rand() & 15) + 1);
+    }
+
+    return true;
+}
+
+bool PixelHarness::check_block_copy_s_p(x265::blockcpy_s_p ref, x265::blockcpy_s_p opt)
+{
+    ALIGN_VAR_16(short, ref_dest[64 * 64]);
+    ALIGN_VAR_16(short, opt_dest[64 * 64]);
+    int bx = 64;
+    int by = 64;
+    int j = 0;
+    for (int i = 0; i <= 100; i++)
+    {
+        opt(bx, by, opt_dest, 64, pbuf2 + j, 128);
+        ref(bx, by, ref_dest, 64, pbuf2 + j, 128);
+
+        if (memcmp(ref_dest, opt_dest, 64 * 64 * sizeof(short)))
+            return false;
+
+        j += 4;
+        bx = 4 * ((rand() & 15) + 1);
+        by = 4 * ((rand() & 15) + 1);
+    }
+
+    return true;
+}
+
+bool PixelHarness::check_block_copy_s_c(x265::blockcpy_s_c ref, x265::blockcpy_s_c opt)
+{
+    ALIGN_VAR_16(short, ref_dest[64 * 64]);
+    ALIGN_VAR_16(short, opt_dest[64 * 64]);
+    int bx = 64;
+    int by = 64;
+    int j = 0;
+    for (int i = 0; i <= 100; i++)
+    {
+        opt(bx, by, opt_dest, 64, (uint8_t*)pbuf2 + j, 128);
+        ref(bx, by, ref_dest, 64, (uint8_t*)pbuf2 + j, 128);
+
+        if (memcmp(ref_dest, opt_dest, 64 * 64 * sizeof(short)))
+            return false;
+
+        j += 4;
+        bx = 4 * ((rand() & 15) + 1);
+        by = 4 * ((rand() & 15) + 1);
+    }
+
+    return true;
+}
+
+bool PixelHarness::check_block_copy_p_s(x265::blockcpy_p_s ref, x265::blockcpy_p_s opt)
+{
+    ALIGN_VAR_16(pixel, ref_dest[64 * 64]);
+    ALIGN_VAR_16(pixel, opt_dest[64 * 64]);
+    int bx = 64;
+    int by = 64;
+    int j = 0;
+    for (int i = 0; i <= 100; i++)
+    {
+        opt(bx, by, opt_dest, 64, (short*)pbuf2 + j, 128);
+        ref(bx, by, ref_dest, 64, (short*)pbuf2 + j, 128);
+
+        if (memcmp(ref_dest, opt_dest, 64 * 64 * sizeof(pixel)))
+            return false;
+
+        j += 4;
+        bx = 4 * ((rand() & 15) + 1);
+        by = 4 * ((rand() & 15) + 1);
+    }
+
+    return true;
+}
+
+bool PixelHarness::check_getResidue(x265::getResidue_t ref, x265::getResidue_t opt)
+{
+    ALIGN_VAR_16(short, ref_dest[64 * 64]);
+    ALIGN_VAR_16(short, opt_dest[64 * 64]);
+    memset(ref_dest, 0, 64 * 64 * sizeof(short));
+    memset(opt_dest, 0, 64 * 64 * sizeof(short));
+    int j = 0;
+    for (int i = 0; i <= 100; i++)
+    {
+        int stride = 64;
+        opt(pbuf1 + j, pbuf2 + j, opt_dest, stride);
+        ref(pbuf1 + j, pbuf2 + j, ref_dest, stride);
+
+        if (memcmp(ref_dest, opt_dest, 64 * 64 * sizeof(short)))
+            return false;
+
+        j += 100;
+    }
+
+    return true;
+}
+
+bool PixelHarness::check_calcRecons(x265::calcRecons_t ref, x265::calcRecons_t opt)
+{
+    ALIGN_VAR_16(short, ref_recq[64 * 64]);
+    ALIGN_VAR_16(short, opt_recq[64 * 64]);
+
+    ALIGN_VAR_16(pixel, ref_reco[64 * 64]);
+    ALIGN_VAR_16(pixel, opt_reco[64 * 64]);
+
+    ALIGN_VAR_16(pixel, ref_pred[64 * 64]);
+    ALIGN_VAR_16(pixel, opt_pred[64 * 64]);
+
+    memset(ref_recq, 0, 64 * 64 * sizeof(short));
+    memset(opt_recq, 0, 64 * 64 * sizeof(short));
+    memset(ref_reco, 0, 64 * 64 * sizeof(pixel));
+    memset(opt_reco, 0, 64 * 64 * sizeof(pixel));
+    memset(ref_pred, 0, 64 * 64 * sizeof(pixel));
+    memset(opt_pred, 0, 64 * 64 * sizeof(pixel));
+
+    int j = 0;
+    for (int i = 0; i <= 100; i++)
+    {
+        int stride = 64;
+        opt(pbuf1 + j, sbuf1 + j, opt_reco, opt_recq, opt_pred, stride, stride, stride);
+        ref(pbuf1 + j, sbuf1 + j, ref_reco, ref_recq, ref_pred, stride, stride, stride);
+
+        if (memcmp(ref_recq, opt_recq, 64 * 64 * sizeof(short)) || memcmp(ref_reco, opt_reco, 64 * 64 * sizeof(pixel)) || memcmp(ref_pred, opt_pred, 64 * 64 * sizeof(pixel)))
+            return false;
+
+        j += 100;
+    }
+
+    return true;
+}
+
+bool PixelHarness::testCorrectness(const EncoderPrimitives& ref, const EncoderPrimitives& opt)
+{
+    for (uint16_t curpar = 0; curpar < NUM_PARTITIONS; curpar++)
+    {
+        if (opt.satd[curpar])
+        {
+            if (!check_pixel_primitive(ref.satd[curpar], opt.satd[curpar]))
+            {
+                printf("satd[%s]: failed!\n", FuncNames[curpar]);
+                return false;
+            }
+        }
+
+        if (opt.sad[curpar])
+        {
+            if (!check_pixel_primitive(ref.sad[curpar], opt.sad[curpar]))
+            {
+                printf("sad[%s]: failed!\n", FuncNames[curpar]);
+                return false;
+            }
+        }
+
+        if (opt.sse_pp[curpar])
+        {
+            if (!check_pixel_primitive(ref.sse_pp[curpar], opt.sse_pp[curpar]))
+            {
+                printf("sse_pp[%s]: failed!\n", FuncNames[curpar]);
+                return false;
+            }
+        }
+
+        if (opt.sse_sp[curpar])
+        {
+            if (!check_pixel_sp_primitive(ref.sse_sp[curpar], opt.sse_sp[curpar]))
+            {
+                printf("sse_sp[%s]: failed!\n", FuncNames[curpar]);
+                return false;
+            }
+        }
+
+        if (opt.sse_ss[curpar])
+        {
+            if (!check_pixel_ss_primitive(ref.sse_ss[curpar], opt.sse_ss[curpar]))
+            {
+                printf("sse_ss[%s]: failed!\n", FuncNames[curpar]);
+                return false;
+            }
+        }
+
+        if (opt.sad_x3[curpar])
+        {
+            if (!check_pixel_primitive_x3(ref.sad_x3[curpar], opt.sad_x3[curpar]))
+            {
+                printf("sad_x3[%s]: failed!\n", FuncNames[curpar]);
+                return false;
+            }
+        }
+
+        if (opt.sad_x4[curpar])
+        {
+            if (!check_pixel_primitive_x4(ref.sad_x4[curpar], opt.sad_x4[curpar]))
+            {
+                printf("sad_x4[%s]: failed!\n", FuncNames[curpar]);
+                return false;
+            }
+        }
+    }
+
+    if (opt.sa8d_8x8)
+    {
+        if (!check_pixel_primitive(ref.sa8d_8x8, opt.sa8d_8x8))
+        {
+            printf("sa8d_8x8: failed!\n");
+            return false;
+        }
+    }
+
+    if (opt.sa8d_16x16)
+    {
+        if (!check_pixel_primitive(ref.sa8d_16x16, opt.sa8d_16x16))
+        {
+            printf("sa8d_16x16: failed!\n");
+            return false;
+        }
+    }
+
+    if (opt.sa8d_32x32)
+    {
+        if (!check_pixel_primitive(ref.sa8d_32x32, opt.sa8d_32x32))
+        {
+            printf("sa8d_32x32: failed!\n");
+            return false;
+        }
+    }
+
+    if (opt.sa8d_64x64)
+    {
+        if (!check_pixel_primitive(ref.sa8d_64x64, opt.sa8d_64x64))
+        {
+            printf("sa8d_64x64: failed!\n");
+            return false;
+        }
+    }
+
+    if (opt.cpyblock)
+    {
+        if (!check_block_copy(ref.cpyblock, opt.cpyblock))
+        {
+            printf("block copy failed!\n");
+            return false;
+        }
+    }
+
+    if (opt.cpyblock_p_s)
+    {
+        if (!check_block_copy_p_s(ref.cpyblock_p_s, opt.cpyblock_p_s))
+        {
+            printf("block copy pixel_short failed!\n");
+            return false;
+        }
+    }
+
+    if (opt.cpyblock_s_p)
+    {
+        if (!check_block_copy_s_p(ref.cpyblock_s_p, opt.cpyblock_s_p))
+        {
+            printf("block copy short_pixel failed!\n");
+            return false;
+        }
+    }
+
+    if (opt.cpyblock_s_c)
+    {
+        if (!check_block_copy_s_c(ref.cpyblock_s_c, opt.cpyblock_s_c))
+        {
+            printf("block copy short_char failed!\n");
+            return false;
+        }
+    }
+
+    for (int i = 0; i <= 8; (i ? i <<= 1 : i++))
+    {
+        if (opt.getResidue[i])
+        {
+            if (!check_getResidue(ref.getResidue[i], opt.getResidue[i]))
+            {
+                printf("getResidue width:%d failed!\n", i ? i * 8 : 4);
+                return false;
+            }
+        }
+    }
+
+    for (int i = 0; i <= 8; (i ? i <<= 1 : i++))
+    {
+        if (opt.calcRecons[i])
+        {
+            if (!check_calcRecons(ref.calcRecons[i], opt.calcRecons[i]))
+            {
+                printf("calcRecon width:%d failed!\n", i ? i * 8 : 4);
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void PixelHarness::measureSpeed(const EncoderPrimitives& ref, const EncoderPrimitives& opt)
+{
+    ALIGN_VAR_16(int, cres[16]);
+    pixel *fref = pbuf2 + 2 * INCR;
+
+    for (int curpar = 0; curpar < NUM_PARTITIONS; curpar++)
+    {
+        if (opt.satd[curpar])
+        {
+            printf("  satd[%s]", FuncNames[curpar]);
+            REPORT_SPEEDUP(opt.satd[curpar], ref.satd[curpar], pbuf1, STRIDE, fref, STRIDE);
+        }
+
+        if (opt.sad[curpar])
+        {
+            printf("   sad[%s]", FuncNames[curpar]);
+            REPORT_SPEEDUP(opt.sad[curpar], ref.sad[curpar], pbuf1, STRIDE, fref, STRIDE);
+        }
+
+        if (opt.sse_pp[curpar])
+        {
+            printf("sse_pp[%s]", FuncNames[curpar]);
+            REPORT_SPEEDUP(opt.sse_pp[curpar], ref.sse_pp[curpar], pbuf1, STRIDE, fref, STRIDE);
+        }
+
+        if (opt.sse_sp[curpar])
+        {
+            printf("sse_sp[%s]", FuncNames[curpar]);
+            REPORT_SPEEDUP(opt.sse_sp[curpar], ref.sse_sp[curpar], (short*)pbuf1, STRIDE, fref, STRIDE);
+        }
+
+        if (opt.sse_ss[curpar])
+        {
+            printf("sse_ss[%s]", FuncNames[curpar]);
+            REPORT_SPEEDUP(opt.sse_ss[curpar], ref.sse_ss[curpar], (short*)pbuf1, STRIDE, (short*)fref, STRIDE);
+        }
+
+        if (opt.sad_x3[curpar])
+        {
+            printf("sad_x3[%s]", FuncNames[curpar]);
+            REPORT_SPEEDUP(opt.sad_x3[curpar], ref.sad_x3[curpar], pbuf1, fref, fref + 1, fref - 1, FENC_STRIDE + 5, &cres[0]);
+        }
+
+        if (opt.sad_x4[curpar])
+        {
+            printf("sad_x4[%s]", FuncNames[curpar]);
+            REPORT_SPEEDUP(opt.sad_x4[curpar], ref.sad_x4[curpar], pbuf1, fref, fref + 1, fref - 1, fref - INCR, FENC_STRIDE + 5, &cres[0]);
+        }
+    }
+
+    if (opt.sa8d_8x8)
+    {
+        printf("sa8d_8x8");
+        REPORT_SPEEDUP(opt.sa8d_8x8, ref.sa8d_8x8, pbuf1, STRIDE, pbuf2, STRIDE);
+    }
+
+    if (opt.sa8d_16x16)
+    {
+        printf("sa8d_16x16");
+        REPORT_SPEEDUP(opt.sa8d_16x16, ref.sa8d_16x16, pbuf1, STRIDE, pbuf2, STRIDE);
+    }
+
+    if (opt.sa8d_32x32)
+    {
+        printf("sa8d_32x32");
+        REPORT_SPEEDUP(opt.sa8d_32x32, ref.sa8d_32x32, pbuf1, STRIDE, pbuf2, STRIDE);
+    }
+
+    if (opt.sa8d_64x64)
+    {
+        printf("sa8d_64x64");
+        REPORT_SPEEDUP(opt.sa8d_64x64, ref.sa8d_64x64, pbuf1, STRIDE, pbuf2, STRIDE);
+    }
+
+    if (opt.cpyblock)
+    {
+        printf("block cpy");
+        REPORT_SPEEDUP(opt.cpyblock, ref.cpyblock, 64, 64, pbuf1, FENC_STRIDE, pbuf2, STRIDE);
+    }
+
+    if (opt.cpyblock_p_s)
+    {
+        printf("p_s   cpy");
+        REPORT_SPEEDUP(opt.cpyblock_p_s, ref.cpyblock_p_s, 64, 64, pbuf1, FENC_STRIDE, (short*)pbuf2, STRIDE);
+    }
+
+    if (opt.cpyblock_s_p)
+    {
+        printf("s_p   cpy");
+        REPORT_SPEEDUP(opt.cpyblock_s_p, ref.cpyblock_s_p, 64, 64, (short*)pbuf1, FENC_STRIDE, pbuf2, STRIDE);
+    }
+
+    if (opt.cpyblock_s_c)
+    {
+        printf("s_c   cpy");
+        REPORT_SPEEDUP(opt.cpyblock_s_c, ref.cpyblock_s_c, 64, 64, (short*)pbuf1, FENC_STRIDE, (uint8_t*)pbuf2, STRIDE);
+    }
+
+    for (int i = 0; i <= 8; i ? i <<= 1 : i++)
+    {
+        if (opt.getResidue[i])
+        {
+            printf("getResidue%dx%d", (i ? i * 8 : 4), (i ? i * 8 : 4));
+            REPORT_SPEEDUP(opt.getResidue[i], ref.getResidue[i], pbuf1, pbuf2, sbuf1, 64);
+        }
+    }
+
+    for (int i = 0; i <= 8; i ? i <<= 1 : i++)
+    {
+        if (opt.calcRecons[i])
+        {
+            printf("calcRecons%dx%d", (i ? i * 8 : 4), (i ? i * 8 : 4));
+            REPORT_SPEEDUP(opt.calcRecons[i], ref.calcRecons[i], pbuf1, sbuf1, pbuf2, sbuf1, pbuf1, 64, 64, 64);
+        }
+    }
+}
