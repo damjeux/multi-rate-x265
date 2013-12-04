05c6198e9477233da2a622ab66c212b7840d687e
Deepthi Devaki <deepthidevaki@multicorewareinc.com>
2013-12-04 13:04:39 +0530

    Enable topskip and earlyexit for all rd levels <= 4 (output changes for presets faster than "slow")
    Also use the encodeResandCalcRDInter instead of the refactored estimate function.

---
+++ source/Lib/TLibEncoder/TEncCu.h
@@ -0,0 +1,198 @@
+/* The copyright in this software is being made available under the BSD
+ * License, included below. This software may be subject to other third party
+ * and contributor rights, including patent rights, and no such rights are
+ * granted under this license.
+ *
+ * Copyright (c) 2010-2013, ITU/ISO/IEC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  * Neither the name of the ITU/ISO/IEC nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \file     TEncCu.h
+    \brief    Coding Unit (CU) encoder class (header)
+*/
+
+#ifndef X265_TENCCU_H
+#define X265_TENCCU_H
+
+#define ANGULAR_MODE_ID 2
+#define AMP_ID 3
+#define INTER_MODES 4
+#define INTRA_MODES 3
+
+#include "TLibCommon/CommonDef.h"
+#include "TLibCommon/TComYuv.h"
+#include "TLibCommon/TComPrediction.h"
+#include "TLibCommon/TComTrQuant.h"
+#include "TLibCommon/TComBitCounter.h"
+#include "TLibCommon/TComDataCU.h"
+#include "TShortYUV.h"
+
+#include "TEncEntropy.h"
+#include "TEncSearch.h"
+
+//! \ingroup TLibEncoder
+//! \{
+struct StatisticLog
+{
+    uint64_t cntInter[4];
+    uint64_t cntIntra[4];
+    uint64_t cuInterDistribution[4][INTER_MODES];
+    uint64_t cuIntraDistribution[4][INTRA_MODES];
+    uint64_t cntIntraNxN;
+    uint64_t cntSkipCu[4];
+    uint64_t cntTotalCu[4];
+    uint64_t TotalCu;
+
+    StatisticLog()
+    {
+        memset(this, 0, sizeof(StatisticLog));
+    }
+};
+
+namespace x265 {
+// private namespace
+
+class Encoder;
+class TEncSbac;
+class TEncCavlc;
+
+// ====================================================================================================================
+// Class definition
+// ====================================================================================================================
+
+/// CU encoder class
+class TEncCu
+{
+private:
+
+    static const int MAX_PRED_TYPES = 6;
+
+    TComDataCU** m_interCU_2Nx2N;
+    TComDataCU** m_interCU_2NxN;
+    TComDataCU** m_interCU_Nx2N;
+    TComDataCU** m_intraInInterCU;
+    TComDataCU** m_mergeCU;
+    TComDataCU** m_bestMergeCU;
+    TComDataCU** m_bestCU;      ///< Best CUs at each depth
+    TComDataCU** m_tempCU;      ///< Temporary CUs at each depth
+
+    TComYuv**    m_bestPredYuv; ///< Best Prediction Yuv for each depth
+    TShortYUV**  m_bestResiYuv; ///< Best Residual Yuv for each depth
+    TComYuv**    m_bestRecoYuv; ///< Best Reconstruction Yuv for each depth
+
+    TComYuv**    m_tmpPredYuv;  ///< Temporary Prediction Yuv for each depth
+    TShortYUV**  m_tmpResiYuv;  ///< Temporary Residual Yuv for each depth
+    TComYuv**    m_tmpRecoYuv;  ///< Temporary Reconstruction Yuv for each depth
+    TComYuv**    m_modePredYuv[MAX_PRED_TYPES]; ///< Prediction buffers for inter, intra, rect(2) and merge
+    TComYuv**    m_bestMergeRecoYuv;
+    TComYuv**    m_origYuv;     ///< Original Yuv at each depth
+
+    TEncCfg*     m_cfg;
+    TEncSearch*  m_search;
+    TComTrQuant* m_trQuant;
+    TComRdCost*  m_rdCost;
+    TEncEntropy* m_entropyCoder;
+    TComBitCounter* m_bitCounter;
+
+    // SBAC RD
+    TEncSbac***  m_rdSbacCoders;
+    TEncSbac*    m_rdGoOnSbacCoder;
+
+    uint32_t     m_LCUPredictionSAD;
+    int          m_addSADDepth;
+    int          m_temporalSAD;
+    UChar        m_totalDepth;
+
+    bool         m_bEncodeDQP;
+    bool         m_abortFlag; // aborts recursion when the child CU costs more than parent CU
+
+public:
+
+    StatisticLog  m_sliceTypeLog[3];
+    StatisticLog* m_log;
+    TEncCu();
+
+    void init(Encoder* top);
+    void create(UChar totalDepth, uint32_t maxWidth);
+    void destroy();
+    void compressCU(TComDataCU* cu);
+    void encodeCU(TComDataCU* cu);
+
+    void setRDSbacCoder(TEncSbac*** rdSbacCoder) { m_rdSbacCoders = rdSbacCoder; }
+
+    void setEntropyCoder(TEncEntropy* entropyCoder) { m_entropyCoder = entropyCoder; }
+
+    void setPredSearch(TEncSearch* predSearch) { m_search = predSearch; }
+
+    void setRDGoOnSbacCoder(TEncSbac* rdGoOnSbacCoder) { m_rdGoOnSbacCoder = rdGoOnSbacCoder; }
+
+    void setTrQuant(TComTrQuant* trQuant) { m_trQuant = trQuant; }
+
+    void setRdCost(TComRdCost* rdCost) { m_rdCost = rdCost; }
+
+    void setBitCounter(TComBitCounter* pcBitCounter) { m_bitCounter = pcBitCounter; }
+
+protected:
+
+    void finishCU(TComDataCU* cu, uint32_t absPartIdx, uint32_t depth);
+    void xCompressCU(TComDataCU*& outBestCU, TComDataCU*& outTempCU, uint32_t depth, PartSize parentSize = SIZE_NONE);
+    void xCompressIntraCU(TComDataCU*& outBestCU, TComDataCU*& outTempCU, uint32_t depth);
+    void xCompressInterCU(TComDataCU*& outBestCU, TComDataCU*& outTempCU, TComDataCU*& cu, uint32_t depth, uint32_t partitionIndex, UChar minDepth);
+    void xEncodeCU(TComDataCU* cu, uint32_t absPartIdx, uint32_t depth);
+    int  xComputeQP(TComDataCU* cu);
+    void xCheckBestMode(TComDataCU*& outBestCU, TComDataCU*& outTempCU, uint32_t depth);
+
+    void xCheckRDCostMerge2Nx2N(TComDataCU*& outBestCU, TComDataCU*& outTempCU, bool *earlyDetectionSkipMode,
+                                TComYuv*& outBestPredYuv, TComYuv*& rpcYuvReconBest);
+    void xComputeCostIntraInInter(TComDataCU* cu, PartSize partSize);
+    void xCheckRDCostInter(TComDataCU*& outBestCU, TComDataCU*& outTempCU, PartSize partSize, bool bUseMRG = false);
+    void xComputeCostInter(TComDataCU* outTempCU, TComYuv* outPredYUV, PartSize partSize, bool bUseMRG = false);
+    void xComputeCostMerge2Nx2N(TComDataCU*& outBestCU, TComDataCU*& outTempCU, TComYuv*& bestPredYuv, TComYuv*& tmpPredYuv);
+    void xEncodeIntraInInter(TComDataCU* cu, TComYuv* fencYuv, TComYuv* predYuv, TShortYUV* outResiYuv, TComYuv* outReconYuv);
+    void xCheckRDCostIntra(TComDataCU*& outBestCU, TComDataCU*& outTempCU, PartSize partSize);
+    void xCheckRDCostIntraInInter(TComDataCU*& outBestCU, TComDataCU*& outTempCU, PartSize partSize);
+    void xCheckDQP(TComDataCU* cu);
+
+    void xCheckIntraPCM(TComDataCU*& outBestCU, TComDataCU*& outTempCU);
+    void xCopyAMVPInfo(AMVPInfo* src, AMVPInfo* dst);
+    void xCopyYuv2Pic(TComPic* outPic, uint32_t cuAddr, uint32_t absPartIdx, uint32_t depth, uint32_t uiSrcDepth, TComDataCU* cu,
+                      uint32_t lpelx, uint32_t tpely);
+    void xCopyYuv2Tmp(uint32_t uhPartUnitIdx, uint32_t depth);
+
+    bool getdQPFlag()        { return m_bEncodeDQP; }
+
+    void setdQPFlag(bool b)  { m_bEncodeDQP = b; }
+
+    void deriveTestModeAMP(TComDataCU* bestCU, PartSize parentSize, bool &bTestAMP_Hor, bool &bTestAMP_Ver,
+                           bool &bTestMergeAMP_Hor, bool &bTestMergeAMP_Ver);
+
+    void xFillPCMBuffer(TComDataCU* outCU, TComYuv* origYuv);
+};
+}
+//! \}
+
+#endif // ifndef X265_TENCCU_H
---
+++ source/common/primitives.cpp
@@ -0,0 +1,163 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#include "TLibCommon/TComRom.h"
+#include "TLibEncoder/TEncSbac.h"
+#include "primitives.h"
+#include "common.h"
+
+#include <assert.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+namespace x265 {
+// x265 private namespace
+
+uint8_t lumaPartitioneMapTable[] =
+{
+//  4          8          12          16          20  24          28  32          36  40  44  48          52  56  60  64
+    LUMA_4x4,  LUMA_4x8,  255,        LUMA_4x16,  255, 255,        255, 255,        255, 255, 255, 255,        255, 255, 255, 255,        // 4
+    LUMA_8x4,  LUMA_8x8,  255,        LUMA_8x16,  255, 255,        255, LUMA_8x32,  255, 255, 255, 255,        255, 255, 255, 255,        // 8
+    255,        255,      255,        LUMA_12x16, 255, 255,        255, 255,        255, 255, 255, 255,        255, 255, 255, 255,        // 12
+    LUMA_16x4, LUMA_16x8, LUMA_16x12, LUMA_16x16, 255, 255,        255, LUMA_16x32, 255, 255, 255, 255,        255, 255, 255, LUMA_16x64, // 16
+    255,        255,      255,        255,        255, 255,        255, 255,        255, 255, 255, 255,        255, 255, 255, 255,        // 20
+    255,        255,      255,        255,        255, 255,        255, LUMA_24x32, 255, 255, 255, 255,        255, 255, 255, 255,        // 24
+    255,        255,      255,        255,        255, 255,        255, 255,        255, 255, 255, 255,        255, 255, 255, 255,        // 28
+    255,        LUMA_32x8, 255,       LUMA_32x16, 255, LUMA_32x24, 255, LUMA_32x32, 255, 255, 255, 255,        255, 255, 255, LUMA_32x64, // 32
+    255,        255,      255,        255,        255, 255,        255, 255,        255, 255, 255, 255,        255, 255, 255, 255,        // 36
+    255,        255,      255,        255,        255, 255,        255, 255,        255, 255, 255, 255,        255, 255, 255, 255,        // 40
+    255,        255,      255,        255,        255, 255,        255, 255,        255, 255, 255, 255,        255, 255, 255, 255,        // 44
+    255,        255,      255,        255,        255, 255,        255, 255,        255, 255, 255, 255,        255, 255, 255, LUMA_48x64, // 48
+    255,        255,      255,        255,        255, 255,        255, 255,        255, 255, 255, 255,        255, 255, 255, 255,        // 52
+    255,        255,      255,        255,        255, 255,        255, 255,        255, 255, 255, 255,        255, 255, 255, 255,        // 56
+    255,        255,      255,        255,        255, 255,        255, 255,        255, 255, 255, 255,        255, 255, 255, 255,        // 60
+    255,        255,      255,        LUMA_64x16, 255, 255,        255, LUMA_64x32, 255, 255, 255, LUMA_64x48, 255, 255, 255, LUMA_64x64  // 64
+};
+
+/* the "authoritative" set of encoder primitives */
+EncoderPrimitives primitives;
+
+void Setup_C_PixelPrimitives(EncoderPrimitives &p);
+void Setup_C_DCTPrimitives(EncoderPrimitives &p);
+void Setup_C_IPFilterPrimitives(EncoderPrimitives &p);
+void Setup_C_IPredPrimitives(EncoderPrimitives &p);
+
+void Setup_C_Primitives(EncoderPrimitives &p)
+{
+    Setup_C_PixelPrimitives(p);      // pixel.cpp
+    Setup_C_DCTPrimitives(p);        // dct.cpp
+    Setup_C_IPFilterPrimitives(p);   // ipfilter.cpp
+    Setup_C_IPredPrimitives(p);      // intrapred.cpp
+}
+}
+
+using namespace x265;
+
+/* cpuid == 0 - auto-detect CPU type, else
+ * cpuid > 0 -  force CPU type
+ * cpuid < 0  - auto-detect if uninitialized */
+extern "C"
+void x265_setup_primitives(x265_param *param, int cpuid)
+{
+    // initialize global variables
+    initROM();
+    buildNextStateTable();
+
+    if (cpuid < 0)
+    {
+        if (primitives.sad[0])
+            return;
+        else
+            cpuid = 0;
+    }
+    if (cpuid == 0)
+    {
+        cpuid = x265::cpu_detect();
+    }
+    if (param->logLevel >= X265_LOG_INFO)
+    {
+        char buf[1000];
+        char *p = buf + sprintf(buf, "using cpu capabilities:");
+        for (int i = 0; x265::cpu_names[i].flags; i++)
+        {
+            if (!strcmp(x265::cpu_names[i].name, "SSE2")
+                && cpuid & (X265_CPU_SSE2_IS_FAST | X265_CPU_SSE2_IS_SLOW))
+                continue;
+            if (!strcmp(x265::cpu_names[i].name, "SSE3")
+                && (cpuid & X265_CPU_SSSE3 || !(cpuid & X265_CPU_CACHELINE_64)))
+                continue;
+            if (!strcmp(x265::cpu_names[i].name, "SSE4.1")
+                && (cpuid & X265_CPU_SSE42))
+                continue;
+            if ((cpuid & x265::cpu_names[i].flags) == x265::cpu_names[i].flags
+                && (!i || x265::cpu_names[i].flags != x265::cpu_names[i - 1].flags))
+                p += sprintf(p, " %s", x265::cpu_names[i].name);
+        }
+
+        if (!cpuid)
+            p += sprintf(p, " none!");
+        x265_log(param, X265_LOG_INFO, "%s\n", buf);
+    }
+
+    Setup_C_Primitives(primitives);
+
+#if ENABLE_VECTOR_PRIMITIVES
+    Setup_Vector_Primitives(primitives, cpuid);
+#endif
+#if ENABLE_ASM_PRIMITIVES
+    Setup_Assembly_Primitives(primitives, cpuid);
+#endif
+
+    primitives.sa8d_inter[LUMA_8x8] = primitives.sa8d[BLOCK_8x8];
+    primitives.sa8d_inter[LUMA_16x16] = primitives.sa8d[BLOCK_16x16];
+    primitives.sa8d_inter[LUMA_32x32] = primitives.sa8d[BLOCK_32x32];
+    primitives.sa8d_inter[LUMA_64x64] = primitives.sa8d[BLOCK_64x64];
+
+    // SA8D devolves to SATD for blocks not even multiples of 8x8
+    primitives.sa8d_inter[LUMA_4x4]   = primitives.satd[LUMA_4x4];
+    primitives.sa8d_inter[LUMA_4x8]   = primitives.satd[LUMA_4x8];
+    primitives.sa8d_inter[LUMA_4x16]  = primitives.satd[LUMA_4x16];
+    primitives.sa8d_inter[LUMA_8x4]   = primitives.satd[LUMA_8x4];
+    primitives.sa8d_inter[LUMA_16x4]  = primitives.satd[LUMA_16x4];
+    primitives.sa8d_inter[LUMA_16x12] = primitives.satd[LUMA_16x12];
+    primitives.sa8d_inter[LUMA_12x16] = primitives.satd[LUMA_12x16];
+
+#if ENABLE_VECTOR_PRIMITIVES && ENABLE_ASM_PRIMITIVES
+    /* no logging if full optimizations are enabled */
+#elif ENABLE_ASM_PRIMITIVES
+    x265_log(param, X265_LOG_INFO, "performance primitives: only assembly\n");
+#elif ENABLE_VECTOR_PRIMITIVES
+    x265_log(param, X265_LOG_INFO, "performance primitives: only intrinsics\n");
+#else
+    x265_log(param, X265_LOG_INFO, "performance primitives: none\n");
+#endif
+}
+
+#if !defined(ENABLE_ASM_PRIMITIVES)
+// the intrinsic primitives will not use MMX instructions, so if assembly
+// is disabled there should be no reason to use EMMS.
+extern "C" void x265_cpu_emms(void) {}
+
+#endif
