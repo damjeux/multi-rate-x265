a04495f83c131094d2426429e2debee858128c9f
Steve Borho <steve@borho.org>
2013-12-05 00:53:59 -0600

    cli: fix final stat line handling of optional PSNR and SSIM stats

---
+++ source/common/lowres.cpp
@@ -0,0 +1,166 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Gopu Govindaswamy <gopu@multicorewareinc.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#include "TLibCommon/TComPic.h"
+#include "lowres.h"
+#include "mv.h"
+
+using namespace x265;
+
+void Lowres::create(TComPicYuv *orig, int bframes, int32_t *aqMode)
+{
+    isLowres = true;
+    width = orig->getWidth() / 2;
+    lines = orig->getHeight() / 2;
+    lumaStride = width + 2 * orig->getLumaMarginX();
+    if (lumaStride & 31)
+        lumaStride += 32 - (lumaStride & 31);
+    int cuWidth = (width + X265_LOWRES_CU_SIZE - 1) >> X265_LOWRES_CU_BITS;
+    int cuHeight = (lines + X265_LOWRES_CU_SIZE - 1) >> X265_LOWRES_CU_BITS;
+    int cuCount = cuWidth * cuHeight;
+
+    /* rounding the width to multiple of lowres CU size */
+    width = cuWidth * X265_LOWRES_CU_SIZE;
+    lines = cuHeight * X265_LOWRES_CU_SIZE;
+
+    if (*aqMode)
+    {
+        qpAqOffset = (double*)x265_malloc(sizeof(double) * cuCount);
+        invQscaleFactor = (int*)x265_malloc(sizeof(int) * cuCount);
+        qpOffset = (double*)x265_malloc(sizeof(double) * cuCount);
+        if (!qpAqOffset || !invQscaleFactor || !qpOffset)
+            *aqMode = 0;
+    }
+    propagateCost = (uint16_t*)x265_malloc(sizeof(uint16_t) * cuCount);
+
+    /* allocate lowres buffers */
+    for (int i = 0; i < 4; i++)
+    {
+        buffer[i] = (Pel*)X265_MALLOC(Pel, lumaStride * (lines + 2 * orig->getLumaMarginY()));
+    }
+
+    int padoffset = lumaStride * orig->getLumaMarginY() + orig->getLumaMarginX();
+    lowresPlane[0] = buffer[0] + padoffset;
+    lowresPlane[1] = buffer[1] + padoffset;
+    lowresPlane[2] = buffer[2] + padoffset;
+    lowresPlane[3] = buffer[3] + padoffset;
+
+    intraCost = (int32_t*)X265_MALLOC(int, cuCount);
+
+    for (int i = 0; i < bframes + 2; i++)
+    {
+        for (int j = 0; j < bframes + 2; j++)
+        {
+            rowSatds[i][j] = (int32_t*)X265_MALLOC(int, cuHeight);
+            lowresCosts[i][j] = (uint16_t*)X265_MALLOC(uint16_t, cuCount);
+        }
+    }
+
+    for (int i = 0; i < bframes + 1; i++)
+    {
+        lowresMvs[0][i] = (MV*)X265_MALLOC(MV, cuCount);
+        lowresMvs[1][i] = (MV*)X265_MALLOC(MV, cuCount);
+        lowresMvCosts[0][i] = (int32_t*)X265_MALLOC(int, cuCount);
+        lowresMvCosts[1][i] = (int32_t*)X265_MALLOC(int, cuCount);
+    }
+}
+
+void Lowres::destroy(int bframes)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        X265_FREE(buffer[i]);
+    }
+
+    X265_FREE(intraCost);
+
+    for (int i = 0; i < bframes + 2; i++)
+    {
+        for (int j = 0; j < bframes + 2; j++)
+        {
+            X265_FREE(rowSatds[i][j]);
+            X265_FREE(lowresCosts[i][j]);
+        }
+    }
+
+    for (int i = 0; i < bframes + 1; i++)
+    {
+        X265_FREE(lowresMvs[0][i]);
+        X265_FREE(lowresMvs[1][i]);
+        X265_FREE(lowresMvCosts[0][i]);
+        X265_FREE(lowresMvCosts[1][i]);
+    }
+
+    X265_FREE(qpAqOffset);
+    X265_FREE(invQscaleFactor);
+    X265_FREE(qpOffset);
+    X265_FREE(propagateCost);
+}
+
+// (re) initialize lowres state
+void Lowres::init(TComPicYuv *orig, int poc, int type, int bframes)
+{
+    bScenecut = true;
+    bIntraCalculated = false;
+    bLastMiniGopBFrame = false;
+    bKeyframe = false; // Not a keyframe unless identified by lookahead
+    sliceType = type;
+    frameNum = poc;
+    leadingBframes = 0;
+    satdCost = -1;
+    memset(costEst, -1, sizeof(costEst));
+
+    if (qpAqOffset && invQscaleFactor)
+        memset(costEstAq, -1, sizeof(costEstAq));
+
+    for (int y = 0; y < bframes + 2; y++)
+    {
+        for (int x = 0; x < bframes + 2; x++)
+        {
+            rowSatds[y][x][0] = -1;
+        }
+    }
+
+    for (int i = 0; i < bframes + 1; i++)
+    {
+        lowresMvs[0][i][0].x = 0x7FFF;
+        lowresMvs[1][i][0].x = 0x7FFF;
+    }
+
+    for (int i = 0; i < bframes + 2; i++)
+    {
+        intraMbs[i] = 0;
+    }
+
+    /* downscale and generate 4 HPEL planes for lookahead */
+    primitives.frame_init_lowres_core(orig->getLumaAddr(),
+                                      lowresPlane[0], lowresPlane[1], lowresPlane[2], lowresPlane[3],
+                                      orig->getStride(), lumaStride, width, lines);
+
+    /* extend hpel planes for motion search */
+    orig->xExtendPicCompBorder(lowresPlane[0], lumaStride, width, lines, orig->getLumaMarginX(), orig->getLumaMarginY());
+    orig->xExtendPicCompBorder(lowresPlane[1], lumaStride, width, lines, orig->getLumaMarginX(), orig->getLumaMarginY());
+    orig->xExtendPicCompBorder(lowresPlane[2], lumaStride, width, lines, orig->getLumaMarginX(), orig->getLumaMarginY());
+    orig->xExtendPicCompBorder(lowresPlane[3], lumaStride, width, lines, orig->getLumaMarginX(), orig->getLumaMarginY());
+    fpelPlane = lowresPlane[0];
+}
---
+++ source/output/y4m.cpp
@@ -0,0 +1,115 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#include "common.h"
+#include "PPA/ppa.h"
+#include "common.h"
+#include "output.h"
+#include "y4m.h"
+
+using namespace x265;
+using namespace std;
+
+Y4MOutput::Y4MOutput(const char *filename, int w, int h, int rate, uint32_t depth, int csp)
+    : width(w)
+    , height(h)
+    , colorSpace(csp)
+    , frameSize(0)
+{
+    ofs.open(filename, ios::binary | ios::out);
+    buf = new char[width];
+
+    const char *cf = (csp >= X265_CSP_I444) ? "444" : (csp >= X265_CSP_I422) ? "422" : "420";
+
+#if HIGH_BIT_DEPTH
+    if (depth > 8)
+    {
+        x265_log(NULL, X265_LOG_WARNING, "y4m: down-shifting reconstructed pixels to 8 bits\n");
+    }
+#else
+    if (depth > 8)
+    {
+        x265_log(NULL, X265_LOG_WARNING, "y4m: forcing reconstructed pixels to 8 bits\n");
+    }
+#endif
+
+    if (ofs)
+    {
+        ofs << "YUV4MPEG2 W" << width << " H" << height << " F" << rate << ":1 Ip" << " C" << cf << "\n";
+        header = ofs.tellp();
+    }
+
+    for (int i = 0; i < x265_cli_csps[colorSpace].planes; i++)
+    {
+        frameSize += (uint32_t)((width >> x265_cli_csps[colorSpace].width[i]) * (height >> x265_cli_csps[colorSpace].height[i]));
+    }
+}
+
+Y4MOutput::~Y4MOutput()
+{
+    ofs.close();
+    delete [] buf;
+}
+
+bool Y4MOutput::writePicture(const x265_picture& pic)
+{
+    PPAStartCpuEventFunc(write_yuv);
+    std::ofstream::pos_type outPicPos = header;
+    outPicPos += pic.poc * (6 + frameSize);
+    ofs.seekp(outPicPos);
+    ofs << "FRAME\n";
+
+#if HIGH_BIT_DEPTH
+    // encoder gave us short pixels, downshift, then write
+    int shift = pic.bitDepth - 8;
+    for (int i = 0; i < x265_cli_csps[colorSpace].planes; i++)
+    {
+        uint16_t *src = (uint16_t*)pic.planes[i];
+        for (int h = 0; h < height >> x265_cli_csps[colorSpace].height[i]; h++)
+        {
+            for (int w = 0; w < width >> x265_cli_csps[colorSpace].width[i]; w++)
+            {
+                buf[w] = (char)(src[w] >> shift);
+            }
+
+            ofs.write(buf, width >> x265_cli_csps[colorSpace].width[i]);
+            src += pic.stride[i];
+        }
+    }
+
+#else // if HIGH_BIT_DEPTH
+    for (int i = 0; i < x265_cli_csps[colorSpace].planes; i++)
+    {
+        char *src = (char*)pic.planes[i];
+        for (int h = 0; h < height >> x265_cli_csps[colorSpace].height[i]; h++)
+        {
+            ofs.write(src, width >> x265_cli_csps[colorSpace].width[i]);
+            src += pic.stride[i];
+        }
+    }
+
+#endif // if HIGH_BIT_DEPTH
+
+    PPAStopCpuEventFunc(write_yuv);
+    return true;
+}
