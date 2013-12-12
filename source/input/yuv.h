5784948a65f0e9a8a61ff7d47aea782490f3b7e5
Sumalatha Polureddy <Sumalatha Polureddy@localhost>
2013-12-12 12:00:25 +0530

    rd level: change the indexing for sa8d threshold array
    The index in threshold array won't be same as depth when the max cu size is less than 64.

---
+++ source/input/yuv.h
@@ -0,0 +1,110 @@
+/*****************************************************************************
+ * Copyright (C) 2013 x265 project
+ *
+ * Authors: Steve Borho <steve@borho.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@multicorewareinc.com.
+ *****************************************************************************/
+
+#ifndef X265_YUV_H
+#define X265_YUV_H
+
+#include "input.h"
+#include "threading.h"
+#include <fstream>
+
+#define QUEUE_SIZE 5
+
+namespace x265 {
+// private x265 namespace
+
+class YUVInput : public Input, public Thread
+{
+protected:
+
+    int width;
+
+    int height;
+
+    int colorSpace; //< source Color Space Parameter
+
+    uint32_t depth;
+
+    uint32_t pixelbytes;
+
+    uint32_t framesize;
+
+    bool threadActive;
+
+    volatile int head;
+
+    volatile int tail;
+
+    bool frameStat[QUEUE_SIZE];
+
+    char* buf[QUEUE_SIZE];
+
+    Event notFull;
+
+    Event notEmpty;
+
+    std::istream *ifs;
+
+public:
+
+    YUVInput(const char *filename, uint32_t inputBitDepth);
+
+    virtual ~YUVInput();
+
+    void setDimensions(int w, int h);
+
+    void setColorSpace(int csp)                   { colorSpace = csp; }
+
+    int getColorSpace() const                     { return colorSpace; }
+
+    void setBitDepth(uint32_t bitDepth)           { depth = bitDepth; }
+
+    float getRate() const                         { return 0.0f; }
+
+    int getWidth() const                          { return width; }
+
+    int getHeight() const                         { return height; }
+
+    bool isEof() const                            { return ifs && ifs->eof();  }
+
+    bool isFail()                                 { return !(ifs && !ifs->fail() && threadActive); }
+
+    void startReader();
+
+    void release();
+
+    int  guessFrameCount();
+
+    void skipFrames(uint32_t numFrames);
+
+    bool readPicture(x265_picture&);
+
+    void threadMain();
+
+    bool populateFrameQueue();
+
+    const char *getName() const                   { return "yuv"; }
+};
+}
+
+#endif // ifndef X265_YUV_H
